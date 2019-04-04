c74fb3be77379f9f7fe575fa96d9ef9ca89cb3ef
Andrea Tarocchi <atarocch@redhat.com>
2019-04-04 11:39:13 +0200

    Project scaffolding

---
+++ README.md
@@ -0,0 +1,8 @@
+# Camel Kafka Connector
+
+## Introduction
+The state is heavly WIP.
+
+This is a "Camel pluggable Kafka connector adapter" that aims to integrate in Kafka Connect all the systems supported by camel components.
+For more information about Kafka Connect take a look [here](http://kafka.apache.org/documentation/#connect).
+
---
+++ core/src/main/java/org/apache/camel/kafkaconnector/CamelSinkConnector.java
@@ -0,0 +1,71 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.kafkaconnector;
+
+import org.apache.camel.CamelContext;
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.connector.Task;
+import org.apache.kafka.connect.sink.SinkConnector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class CamelSinkConnector extends SinkConnector {
+   private static Logger log = LoggerFactory.getLogger(CamelSinkConnector.class);
+   private Map<String, String> configProps;
+
+   private CamelContext camelContext;
+
+   @Override
+   public String version() {
+      return VersionUtil.getVersion();
+   }
+
+   @Override
+   public void start(Map<String, String> map) {
+      configProps = map;
+      //TODO init camelcontext
+   }
+
+   @Override
+   public Class<? extends Task> taskClass() {
+      return CamelSinkTask.class;
+   }
+
+   @Override
+   public List<Map<String, String>> taskConfigs(int maxTasks) {
+      log.info("Setting task configurations for {} workers.", maxTasks);
+      final List<Map<String, String>> configs = new ArrayList<>(maxTasks);
+      for (int i = 0; i < maxTasks; ++i) {
+         configs.add(configProps);
+      }
+      return configs;
+   }
+
+   @Override
+   public void stop() {
+      //TODO stop camelcontext
+   }
+
+   @Override
+   public ConfigDef config() {
+      return CamelSinkConnectorConfig.conf();
+   }
+}
