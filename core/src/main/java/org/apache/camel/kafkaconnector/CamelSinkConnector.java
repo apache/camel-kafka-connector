4fb86d7cdf5ebefaba5d675acd26891869d7a913
Andrea Tarocchi <atarocch@redhat.com>
2019-04-19 23:49:47 +0200

    Merged relevant parts from https://github.com/scholzj/kafka-connect-camel-connector

--- core/src/main/java/org/apache/camel/kafkaconnector/CamelSinkConnector.java
+++ core/src/main/java/org/apache/camel/kafkaconnector/CamelSinkConnector.java
@@ -16,7 +16,7 @@
  */
 package org.apache.camel.kafkaconnector;
 
-import org.apache.camel.CamelContext;
+import org.apache.kafka.common.config.AbstractConfig;
 import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.connect.connector.Task;
 import org.apache.kafka.connect.sink.SinkConnector;
@@ -24,14 +24,28 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class CamelSinkConnector extends SinkConnector {
    private static Logger log = LoggerFactory.getLogger(CamelSinkConnector.class);
-   private Map<String, String> configProps;
 
-   private CamelContext camelContext;
+   public static final String NAME_CONFIG = "name";
+   public static final String COMPONENT_CONFIG = "component";
+   public static final String ADDRESS_CONFIG = "address";
+   public static final String OPTIONS_CONFIG = "options";
+
+   private static final ConfigDef CONFIG_DEF = new ConfigDef()
+           .define(COMPONENT_CONFIG, ConfigDef.Type.STRING, ConfigDef.Importance.HIGH, "Name of the Apache Camel component which will be used to route messages. (component://address?option1=value1&options2=value2)")
+           .define(ADDRESS_CONFIG, ConfigDef.Type.STRING, ConfigDef.Importance.HIGH, "The address which will be used to configure the component. (component://address?option1=value1&options2=value2)")
+           .define(OPTIONS_CONFIG, ConfigDef.Type.LIST, Collections.EMPTY_LIST, ConfigDef.Importance.HIGH, "List of options for configuring the component. The list should contain key-value pairs. E.g. `[\"option1=key1\", \"option2=key2\"]`.");
+
+   private String connectorName;
+   private String component;
+   private String address;
+   private List<String> options;
 
    @Override
    public String version() {
@@ -39,9 +53,16 @@ public class CamelSinkConnector extends SinkConnector {
    }
 
    @Override
-   public void start(Map<String, String> map) {
-      configProps = map;
-      //TODO init camelcontext
+   public void start(Map<String, String> props) {
+      log.info("Connector config keys: {}", String.join(", ", props.keySet()));
+
+      connectorName = props.get(NAME_CONFIG);
+      log.info("Starting connector {}", connectorName);
+
+      AbstractConfig parsedConfig = new AbstractConfig(CONFIG_DEF, props);
+      component = parsedConfig.getString(COMPONENT_CONFIG);
+      address = parsedConfig.getString(ADDRESS_CONFIG);
+      options = parsedConfig.getList(OPTIONS_CONFIG);
    }
 
    @Override
@@ -52,16 +73,26 @@ public class CamelSinkConnector extends SinkConnector {
    @Override
    public List<Map<String, String>> taskConfigs(int maxTasks) {
       log.info("Setting task configurations for {} workers.", maxTasks);
-      final List<Map<String, String>> configs = new ArrayList<>(maxTasks);
-      for (int i = 0; i < maxTasks; ++i) {
-         configs.add(configProps);
+
+      ArrayList<Map<String, String>> configs = new ArrayList<>();
+
+      Map<String, String> config = new HashMap<>(4);
+      config.put(COMPONENT_CONFIG, component);
+      config.put(ADDRESS_CONFIG, address);
+      config.put(OPTIONS_CONFIG, String.join("&", options));
+
+      for (int i = 0; i < maxTasks; i++) {
+         Map<String, String> taskConfig = new HashMap<>(config);
+         taskConfig.put(NAME_CONFIG, connectorName + "-" + i);
+
+         configs.add(taskConfig);
       }
       return configs;
    }
 
    @Override
    public void stop() {
-      //TODO stop camelcontext
+      //nothing to do
    }
 
    @Override
