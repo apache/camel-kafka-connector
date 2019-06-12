8f07aaa5b1fd799907b498d8aecd00534d70102e
Andrea Cosentino <ancosen@gmail.com>
2019-06-12 11:34:24 +0200

    Indentation

--- core/src/main/java/org/apache/camel/kafkaconnector/CamelSourceTask.java
+++ core/src/main/java/org/apache/camel/kafkaconnector/CamelSourceTask.java
@@ -40,135 +40,135 @@ import java.util.List;
 import java.util.Map;
 
 public class CamelSourceTask extends SourceTask {
-	private static final Logger log = LoggerFactory.getLogger(CamelSourceTask.class);
-	private static final String LOCAL_URL = "direct:end";
+    private static final Logger log = LoggerFactory.getLogger(CamelSourceTask.class);
+    private static final String LOCAL_URL = "direct:end";
 
-	private CamelSourceConnectorConfig config;
-	private CamelContext camel;
-	private PollingConsumer consumer;
-	private String topic;
+    private CamelSourceConnectorConfig config;
+    private CamelContext camel;
+    private PollingConsumer consumer;
+    private String topic;
 
-	@Override
-	public String version() {
-		return new CamelSourceConnector().version();
-	}
+    @Override
+    public String version() {
+        return new CamelSourceConnector().version();
+    }
 
-	@Override
-	public void start(Map<String, String> props) {
-		try {
-			log.info("Starting CamelSourceTask connector task");
-			config = new CamelSourceConnectorConfig(props);
+    @Override
+    public void start(Map<String, String> props) {
+        try {
+            log.info("Starting CamelSourceTask connector task");
+            config = new CamelSourceConnectorConfig(props);
 
-			final String remoteUrl = config.getString(CamelSourceConnectorConfig.CAMEL_SOURCE_URL_CONF);
-			topic = config.getString(CamelSourceConnectorConfig.TOPIC_CONF);
+            final String remoteUrl = config.getString(CamelSourceConnectorConfig.CAMEL_SOURCE_URL_CONF);
+            topic = config.getString(CamelSourceConnectorConfig.TOPIC_CONF);
 
-			camel = new DefaultCamelContext();
+            camel = new DefaultCamelContext();
 
-			log.info("Creating Camel route from({}).to({})", remoteUrl, LOCAL_URL);
-			camel.addRoutes(new RouteBuilder() {
-				public void configure() {
-					from(remoteUrl).to(LOCAL_URL);
-				}
-			});
+            log.info("Creating Camel route from({}).to({})", remoteUrl, LOCAL_URL);
+            camel.addRoutes(new RouteBuilder() {
+                public void configure() {
+                    from(remoteUrl).to(LOCAL_URL);
+                }
+            });
 
-			// TODO: Add option to configure pollingConsumerQueueSize,
-			// pollingConsumerBlockWhenFull and pollingConsumerBlockTimeout in LOCAL_URL
+            // TODO: Add option to configure pollingConsumerQueueSize,
+            // pollingConsumerBlockWhenFull and pollingConsumerBlockTimeout in
+            // LOCAL_URL
 
-			Endpoint endpoint = camel.getEndpoint(LOCAL_URL);
-			consumer = endpoint.createPollingConsumer();
-			consumer.start();
+            Endpoint endpoint = camel.getEndpoint(LOCAL_URL);
+            consumer = endpoint.createPollingConsumer();
+            consumer.start();
 
-			log.info("Starting CamelContext");
-			camel.start();
-			log.info("CamelContext started");
-			log.info("CamelSourceTask connector task started");
-		} catch (Exception e) {
-			throw new ConnectException("Failed to create and start Camel context", e);
-		}
-	}
+            log.info("Starting CamelContext");
+            camel.start();
+            log.info("CamelContext started");
+            log.info("CamelSourceTask connector task started");
+        } catch (Exception e) {
+            throw new ConnectException("Failed to create and start Camel context", e);
+        }
+    }
 
-	@Override
-	public List<SourceRecord> poll() {
-		List<SourceRecord> records = new ArrayList<>();
+    @Override
+    public List<SourceRecord> poll() {
+        List<SourceRecord> records = new ArrayList<>();
 
-		Exchange exchange = consumer.receiveNoWait();
+        Exchange exchange = consumer.receiveNoWait();
 
-		if (exchange == null) {
-			return null;
-		}
+        if (exchange == null) {
+            return null;
+        }
 
-		log.info("Received exchange with");
-		log.info("\t from endpoint: {}", exchange.getFromEndpoint());
-		log.info("\t exchange id: {}", exchange.getExchangeId());
-		log.info("\t message id: {}", exchange.getMessage().getMessageId());
-		log.info("\t message body: {}", exchange.getMessage().getBody());
-		log.info("\t message headers: {}", exchange.getMessage().getHeaders());
+        log.info("Received exchange with");
+        log.info("\t from endpoint: {}", exchange.getFromEndpoint());
+        log.info("\t exchange id: {}", exchange.getExchangeId());
+        log.info("\t message id: {}", exchange.getMessage().getMessageId());
+        log.info("\t message body: {}", exchange.getMessage().getBody());
+        log.info("\t message headers: {}", exchange.getMessage().getHeaders());
 
-		// TODO: see if there is a better way to use sourcePartition and sourceOffset
-		Map<String, String> sourcePartition = Collections.singletonMap("filename",
-				exchange.getFromEndpoint().toString());
-		Map<String, String> sourceOffset = Collections.singletonMap("position", exchange.getExchangeId());
+        // TODO: see if there is a better way to use sourcePartition and
+        // sourceOffset
+        Map<String, String> sourcePartition = Collections.singletonMap("filename", exchange.getFromEndpoint().toString());
+        Map<String, String> sourceOffset = Collections.singletonMap("position", exchange.getExchangeId());
 
-		SourceRecord record = new SourceRecord(sourcePartition, sourceOffset, topic, Schema.BYTES_SCHEMA,
-				exchange.getMessage().getBody());
-		setHeaders(record, exchange);
-		records.add(record);
+        SourceRecord record = new SourceRecord(sourcePartition, sourceOffset, topic, Schema.BYTES_SCHEMA, exchange.getMessage().getBody());
+        setHeaders(record, exchange);
+        records.add(record);
 
-		return records;
-	}
+        return records;
+    }
 
-	@Override
-	public void stop() {
-		try {
-			consumer.stop();
-		} catch (Exception e) {
-			throw new ConnectException("Failed to stop Polling Consumer", e);
-		}
-		try {
-			camel.stop();
-		} catch (Exception e) {
-			throw new ConnectException("Failed to stop Camel context", e);
-		}
-	}
+    @Override
+    public void stop() {
+        try {
+            consumer.stop();
+        } catch (Exception e) {
+            throw new ConnectException("Failed to stop Polling Consumer", e);
+        }
+        try {
+            camel.stop();
+        } catch (Exception e) {
+            throw new ConnectException("Failed to stop Camel context", e);
+        }
+    }
 
-	private void setHeaders(SourceRecord record, Exchange exchange) {
-		if (!exchange.getMessage().hasHeaders()) {
-			return;
-		}
+    private void setHeaders(SourceRecord record, Exchange exchange) {
+        if (!exchange.getMessage().hasHeaders()) {
+            return;
+        }
 
-		for (Map.Entry<String, Object> entry : exchange.getMessage().getHeaders().entrySet()) {
-			String key = entry.getKey();
-			Object value = entry.getValue();
+        for (Map.Entry<String, Object> entry : exchange.getMessage().getHeaders().entrySet()) {
+            String key = entry.getKey();
+            Object value = entry.getValue();
 
-			if (value instanceof String) {
-				record.headers().addString(key, (String) value);
-			} else if (value instanceof Boolean) {
-				record.headers().addBoolean(key, (boolean) value);
-			} else if (value instanceof Byte) {
-				record.headers().addByte(key, (byte) value);
-			} else if (value instanceof Byte[]) {
-				record.headers().addBytes(key, (byte[]) value);
-			} else if (value instanceof Date) {
-				SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd");
-				String convertedDate = sdf.format(value);
-				record.headers().addString(key, (String) convertedDate);
-			} else if (value instanceof BigDecimal) {
-				record.headers().addDecimal(key, (BigDecimal) value);
-			} else if (value instanceof Double) {
-				record.headers().addDouble(key, (double) value);
-			} else if (value instanceof Float) {
-				record.headers().addFloat(key, (float) value);
-			} else if (value instanceof Integer) {
-				record.headers().addInt(key, (int) value);
-			} else if (value instanceof Long) {
-				record.headers().addLong(key, (long) value);
-			} else if (value instanceof Short) {
-				record.headers().addShort(key, (short) value);
-			} else if (value instanceof Time) {
-				record.headers().addTime(key, (Time) value);
-			} else if (value instanceof Timestamp) {
-				record.headers().addTimestamp(key, (Timestamp) value);
-			}
-		}
-	}
+            if (value instanceof String) {
+                record.headers().addString(key, (String)value);
+            } else if (value instanceof Boolean) {
+                record.headers().addBoolean(key, (boolean)value);
+            } else if (value instanceof Byte) {
+                record.headers().addByte(key, (byte)value);
+            } else if (value instanceof Byte[]) {
+                record.headers().addBytes(key, (byte[])value);
+            } else if (value instanceof Date) {
+                SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd");
+                String convertedDate = sdf.format(value);
+                record.headers().addString(key, (String)convertedDate);
+            } else if (value instanceof BigDecimal) {
+                record.headers().addDecimal(key, (BigDecimal)value);
+            } else if (value instanceof Double) {
+                record.headers().addDouble(key, (double)value);
+            } else if (value instanceof Float) {
+                record.headers().addFloat(key, (float)value);
+            } else if (value instanceof Integer) {
+                record.headers().addInt(key, (int)value);
+            } else if (value instanceof Long) {
+                record.headers().addLong(key, (long)value);
+            } else if (value instanceof Short) {
+                record.headers().addShort(key, (short)value);
+            } else if (value instanceof Time) {
+                record.headers().addTime(key, (Time)value);
+            } else if (value instanceof Timestamp) {
+                record.headers().addTimestamp(key, (Timestamp)value);
+            }
+        }
+    }
 }
