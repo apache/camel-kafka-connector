39a5cc013d81db2ac034ca721c5063f9275d40ce
Andrea Cosentino <ancosen@gmail.com>
2019-06-12 16:33:55 +0200

    test(source): First attempt of test for Souce

---
+++ core/src/test/java/org/apache/camel/kafkaconnector/CamelSourceTaskTestIT.java
@@ -0,0 +1,58 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.kafkaconnector;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.kafka.connect.header.Header;
+import org.apache.kafka.connect.header.Headers;
+import org.apache.kafka.connect.source.SourceRecord;
+import org.junit.Test;
+
+public class CamelSourceTaskTestIT {
+   
+   @Test
+   public void testUseProto() throws InterruptedException {
+      Map<String, String> props = new HashMap<>();
+      props.put("camel.source.url", "timer:kafkaconnector");
+      props.put("camel.source.kafka.topic", "mytopic");
+
+      CamelSourceTask camelSourceTask = new CamelSourceTask();
+      camelSourceTask.start(props);
+
+      Thread.sleep(5000L);
+      List<SourceRecord> poll = camelSourceTask.poll();
+      assertEquals(1, poll.size());
+      assertEquals("mytopic", poll.get(0).topic());
+      Headers headers = poll.get(0).headers();
+      boolean containsHeader = false;
+      for (Iterator iterator = headers.iterator(); iterator.hasNext();) {
+        Header header = (Header)iterator.next();
+        if (header.key().equalsIgnoreCase("CamelPropertyCamelTimerPeriod")) {
+            containsHeader = true;
+            break;
+        }
+    }
+      assertTrue(containsHeader);
+   }
+}
