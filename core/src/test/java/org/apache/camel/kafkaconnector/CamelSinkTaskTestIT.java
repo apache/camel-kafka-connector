4aafdf61102c33fabe3ea0c81987f0cd42c7ee7b
Andrea Cosentino <ancosen@gmail.com>
2019-06-14 10:00:30 +0200

    test(sink): Properties and headers mixed tests

--- core/src/test/java/org/apache/camel/kafkaconnector/CamelSinkTaskTestIT.java
+++ core/src/test/java/org/apache/camel/kafkaconnector/CamelSinkTaskTestIT.java
@@ -101,4 +101,106 @@ public class CamelSinkTaskTestIT {
         assertEquals(myInteger, exchange.getIn().getHeader("CamelHeaderMyInteger"));
         assertEquals(myLong, exchange.getIn().getHeader("CamelHeaderMyLong", Long.class));
     }
+    
+    @Test
+    public void testBodyAndProperties() throws JsonProcessingException, InterruptedException {
+        Map<String, String> props = new HashMap<>();
+        props.put("camel.sink.url", "seda:test");
+        props.put("topics", "mytopic");
+
+        CamelSinkTask camelsinkTask = new CamelSinkTask();
+        camelsinkTask.start(props);
+
+        String topic = "mytopic";
+        Byte myByte = new Byte("100");
+        Float myFloat = new Float("100");
+        Short myShort = new Short("100");
+        Double myDouble = new Double("100");
+        int myInteger = 100;
+        Long myLong = new Long("100");
+
+        List<SinkRecord> records = new ArrayList<SinkRecord>();
+        SinkRecord record = new SinkRecord(topic, 1, null, "test", null, "camel", 42);
+        record.headers().addBoolean("CamelPropertyMyBoolean", true);
+        record.headers().addByte("CamelPropertyMyByte", myByte);
+        record.headers().addFloat("CamelPropertyMyFloat", myFloat);
+        record.headers().addShort("CamelPropertyMyShort", myShort);
+        record.headers().addDouble("CamelPropertyMyDouble", myDouble);
+        record.headers().addInt("CamelPropertyMyInteger", myInteger);
+        record.headers().addLong("CamelPropertyMyLong", myLong);
+        records.add(record);
+        camelsinkTask.put(records);
+
+        Thread.sleep(1000L);
+        CamelContext context = camelsinkTask.getContext();
+        ConsumerTemplate c = context.createConsumerTemplate();
+        Exchange exchange = c.receiveNoWait("seda:test");
+        assertEquals("camel", exchange.getMessage().getBody());
+        assertEquals("test", exchange.getMessage().getHeaders().get(CamelSinkTask.KAFKA_RECORD_KEY_HEADER));
+        assertTrue((boolean) exchange.getProperties().get(("CamelPropertyMyBoolean")));
+        assertEquals(myByte, (Byte) exchange.getProperties().get("CamelPropertyMyByte"));
+        assertEquals(myFloat, (Float) exchange.getProperties().get("CamelPropertyMyFloat"));
+        assertEquals(myShort, (Short) exchange.getProperties().get("CamelPropertyMyShort"));
+        assertEquals(myDouble, (Double) exchange.getProperties().get("CamelPropertyMyDouble"));
+        assertEquals(myInteger, exchange.getProperties().get("CamelPropertyMyInteger"));
+        assertEquals(myLong, (Long) exchange.getProperties().get("CamelPropertyMyLong"));
+    }
+    
+    @Test
+    public void testBodyAndPropertiesHeadersMixed() throws JsonProcessingException, InterruptedException {
+        Map<String, String> props = new HashMap<>();
+        props.put("camel.sink.url", "seda:test");
+        props.put("topics", "mytopic");
+
+        CamelSinkTask camelsinkTask = new CamelSinkTask();
+        camelsinkTask.start(props);
+
+        String topic = "mytopic";
+        Byte myByte = new Byte("100");
+        Float myFloat = new Float("100");
+        Short myShort = new Short("100");
+        Double myDouble = new Double("100");
+        int myInteger = 100;
+        Long myLong = new Long("100");
+
+        List<SinkRecord> records = new ArrayList<SinkRecord>();
+        SinkRecord record = new SinkRecord(topic, 1, null, "test", null, "camel", 42);
+        record.headers().addBoolean("CamelPropertyMyBoolean", true);
+        record.headers().addByte("CamelPropertyMyByte", myByte);
+        record.headers().addFloat("CamelPropertyMyFloat", myFloat);
+        record.headers().addShort("CamelPropertyMyShort", myShort);
+        record.headers().addDouble("CamelPropertyMyDouble", myDouble);
+        record.headers().addInt("CamelPropertyMyInteger", myInteger);
+        record.headers().addLong("CamelPropertyMyLong", myLong);
+        record.headers().addBoolean("CamelHeaderMyBoolean", true);
+        record.headers().addByte("CamelHeaderMyByte", myByte);
+        record.headers().addFloat("CamelHeaderMyFloat", myFloat);
+        record.headers().addShort("CamelHeaderMyShort", myShort);
+        record.headers().addDouble("CamelHeaderMyDouble", myDouble);
+        record.headers().addInt("CamelHeaderMyInteger", myInteger);
+        record.headers().addLong("CamelHeaderMyLong", myLong);
+        records.add(record);
+        camelsinkTask.put(records);
+
+        Thread.sleep(1000L);
+        CamelContext context = camelsinkTask.getContext();
+        ConsumerTemplate c = context.createConsumerTemplate();
+        Exchange exchange = c.receiveNoWait("seda:test");
+        assertEquals("camel", exchange.getMessage().getBody());
+        assertEquals("test", exchange.getMessage().getHeaders().get(CamelSinkTask.KAFKA_RECORD_KEY_HEADER));
+        assertTrue((boolean) exchange.getProperties().get(("CamelPropertyMyBoolean")));
+        assertEquals(myByte, (Byte) exchange.getProperties().get("CamelPropertyMyByte"));
+        assertEquals(myFloat, (Float) exchange.getProperties().get("CamelPropertyMyFloat"));
+        assertEquals(myShort, (Short) exchange.getProperties().get("CamelPropertyMyShort"));
+        assertEquals(myDouble, (Double) exchange.getProperties().get("CamelPropertyMyDouble"));
+        assertEquals(myInteger, exchange.getProperties().get("CamelPropertyMyInteger"));
+        assertEquals(myLong, (Long) exchange.getProperties().get("CamelPropertyMyLong"));
+        assertTrue(exchange.getIn().getHeader("CamelHeaderMyBoolean", Boolean.class));
+        assertEquals(myByte, exchange.getIn().getHeader("CamelHeaderMyByte", Byte.class));
+        assertEquals(myFloat, exchange.getIn().getHeader("CamelHeaderMyFloat", Float.class));
+        assertEquals(myShort, exchange.getIn().getHeader("CamelHeaderMyShort", Short.class));
+        assertEquals(myDouble, exchange.getIn().getHeader("CamelHeaderMyDouble", Double.class));
+        assertEquals(myInteger, exchange.getIn().getHeader("CamelHeaderMyInteger"));
+        assertEquals(myLong, exchange.getIn().getHeader("CamelHeaderMyLong", Long.class));
+    }
 }
