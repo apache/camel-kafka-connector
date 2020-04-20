47dedf2dc03b9fa3fa97aaf3ef3969e7af718bf0
Andrea Cosentino <ancosen@gmail.com>
2020-04-20 13:18:18 +0200

    Cleanup unused imports

--- core/src/main/java/org/apache/camel/kafkaconnector/CamelSinkTask.java
+++ core/src/main/java/org/apache/camel/kafkaconnector/CamelSinkTask.java
@@ -19,11 +19,9 @@ package org.apache.camel.kafkaconnector;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.ProducerTemplate;
