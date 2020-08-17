2a262037bb205d2bf05b2fefa40a6d8d2e873979
Otavio Rodolfo Piske <opiske@redhat.com>
2020-08-17 10:40:33 +0200

    Burn-in script fixes
    - Fix incorrect path to the mvnw script on the test burn-in script
    - Adjust build target for the multi-module project
    - Ensures that the code is built prior to running the test
    - Make sure to propagate the error code when there is a test failure

--- tests/testBurnInRun.sh
+++ tests/testBurnInRun.sh
@@ -2,15 +2,17 @@
 # Run this script from the project root directory.
 # It will run core module tests in burn in mode (i.e. untill they fail or this scritp is manually terminated)
 
+echo "Building the code before running the burn-in"
+mvn clean compile test-compile
 repeat=${1:-99}
 for (( i = 1; i < $repeat ; i++ ))
 do
   echo "Attempt $i of $repeat"
-  ./mvnw -DskipIntegrationTests=false test -pl tests -am
+  ../mvnw -DskipIntegrationTests=false verify
   exitcode=$?
   if [ $exitcode -ne 0 ]
   then
     echo "Error at attempt $i"
-    exit
+    exit $exitcode
   fi
 done
