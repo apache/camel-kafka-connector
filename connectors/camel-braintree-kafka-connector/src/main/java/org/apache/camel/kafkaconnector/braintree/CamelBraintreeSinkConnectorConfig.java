b39be7b0c4ee2288a4dd14a54c6eb331c569ff5c
Andrea Cosentino <ancosen@gmail.com>
2020-09-30 08:21:28 +0200

    Align to 3.6.0-SNAPSHOT

--- connectors/camel-braintree-kafka-connector/src/main/java/org/apache/camel/kafkaconnector/braintree/CamelBraintreeSinkConnectorConfig.java
+++ connectors/camel-braintree-kafka-connector/src/main/java/org/apache/camel/kafkaconnector/braintree/CamelBraintreeSinkConnectorConfig.java
@@ -27,10 +27,10 @@ public class CamelBraintreeSinkConnectorConfig
             CamelSinkConnectorConfig {
 
     public static final String CAMEL_SINK_BRAINTREE_PATH_API_NAME_CONF = "camel.sink.path.apiName";
-    public static final String CAMEL_SINK_BRAINTREE_PATH_API_NAME_DOC = "What kind of operation to perform One of: [ADDON] [ADDRESS] [CLIENTTOKEN] [CREDITCARDVERIFICATION] [CUSTOMER] [DISCOUNT] [DISPUTE] [DOCUMENTUPLOAD] [MERCHANTACCOUNT] [PAYMENTMETHOD] [PAYMENTMETHODNONCE] [PLAN] [REPORT] [SETTLEMENTBATCHSUMMARY] [SUBSCRIPTION] [TRANSACTION] [WEBHOOKNOTIFICATION]";
+    public static final String CAMEL_SINK_BRAINTREE_PATH_API_NAME_DOC = "What kind of operation to perform One of: [ADD_ON] [ADDRESS] [CLIENT_TOKEN] [CREDIT_CARD_VERIFICATION] [CUSTOMER] [DISCOUNT] [DISPUTE] [DOCUMENT_UPLOAD] [MERCHANT_ACCOUNT] [PAYMENT_METHOD] [PAYMENT_METHOD_NONCE] [PLAN] [REPORT] [SETTLEMENT_BATCH_SUMMARY] [SUBSCRIPTION] [TRANSACTION] [WEBHOOK_NOTIFICATION]";
     public static final String CAMEL_SINK_BRAINTREE_PATH_API_NAME_DEFAULT = null;
     public static final String CAMEL_SINK_BRAINTREE_PATH_METHOD_NAME_CONF = "camel.sink.path.methodName";
-    public static final String CAMEL_SINK_BRAINTREE_PATH_METHOD_NAME_DOC = "What sub operation to use for the selected operation";
+    public static final String CAMEL_SINK_BRAINTREE_PATH_METHOD_NAME_DOC = "What sub operation to use for the selected operation One of: [accept] [addFileEvidence] [addTextEvidence] [cancel] [cancelRelease] [cloneTransaction] [create] [createForCurrency] [credit] [delete] [fetchMerchantAccounts] [finalize] [find] [generate] [grant] [holdInEscrow] [parse] [refund] [releaseFromEscrow] [removeEvidence] [retryCharge] [revoke] [sale] [search] [submitForPartialSettlement] [submitForSettlement] [transactionLevelFees] [update] [updateDetails] [verify] [voidTransaction]";
     public static final String CAMEL_SINK_BRAINTREE_PATH_METHOD_NAME_DEFAULT = null;
     public static final String CAMEL_SINK_BRAINTREE_ENDPOINT_ENVIRONMENT_CONF = "camel.sink.endpoint.environment";
     public static final String CAMEL_SINK_BRAINTREE_ENDPOINT_ENVIRONMENT_DOC = "The environment Either SANDBOX or PRODUCTION";
@@ -38,12 +38,12 @@ public class CamelBraintreeSinkConnectorConfig
     public static final String CAMEL_SINK_BRAINTREE_ENDPOINT_IN_BODY_CONF = "camel.sink.endpoint.inBody";
     public static final String CAMEL_SINK_BRAINTREE_ENDPOINT_IN_BODY_DOC = "Sets the name of a parameter to be passed in the exchange In Body";
     public static final String CAMEL_SINK_BRAINTREE_ENDPOINT_IN_BODY_DEFAULT = null;
-    public static final String CAMEL_SINK_BRAINTREE_ENDPOINT_MERCHANT_ID_CONF = "camel.sink.endpoint.merchantId";
-    public static final String CAMEL_SINK_BRAINTREE_ENDPOINT_MERCHANT_ID_DOC = "The merchant id provided by Braintree.";
-    public static final String CAMEL_SINK_BRAINTREE_ENDPOINT_MERCHANT_ID_DEFAULT = null;
     public static final String CAMEL_SINK_BRAINTREE_ENDPOINT_LAZY_START_PRODUCER_CONF = "camel.sink.endpoint.lazyStartProducer";
     public static final String CAMEL_SINK_BRAINTREE_ENDPOINT_LAZY_START_PRODUCER_DOC = "Whether the producer should be started lazy (on the first message). By starting lazy you can use this to allow CamelContext and routes to startup in situations where a producer may otherwise fail during starting and cause the route to fail being started. By deferring this startup to be lazy then the startup failure can be handled during routing messages via Camel's routing error handlers. Beware that when the first message is processed then creating and starting the producer may take a little time and prolong the total processing time of the processing.";
     public static final Boolean CAMEL_SINK_BRAINTREE_ENDPOINT_LAZY_START_PRODUCER_DEFAULT = false;
+    public static final String CAMEL_SINK_BRAINTREE_ENDPOINT_MERCHANT_ID_CONF = "camel.sink.endpoint.merchantId";
+    public static final String CAMEL_SINK_BRAINTREE_ENDPOINT_MERCHANT_ID_DOC = "The merchant id provided by Braintree.";
+    public static final String CAMEL_SINK_BRAINTREE_ENDPOINT_MERCHANT_ID_DEFAULT = null;
     public static final String CAMEL_SINK_BRAINTREE_ENDPOINT_BASIC_PROPERTY_BINDING_CONF = "camel.sink.endpoint.basicPropertyBinding";
     public static final String CAMEL_SINK_BRAINTREE_ENDPOINT_BASIC_PROPERTY_BINDING_DOC = "Whether the endpoint should use basic property binding (Camel 2.x) or the newer property binding with additional capabilities";
     public static final Boolean CAMEL_SINK_BRAINTREE_ENDPOINT_BASIC_PROPERTY_BINDING_DEFAULT = false;
@@ -100,11 +100,11 @@ public class CamelBraintreeSinkConnectorConfig
     public static ConfigDef conf() {
         ConfigDef conf = new ConfigDef(CamelSinkConnectorConfig.conf());
         conf.define(CAMEL_SINK_BRAINTREE_PATH_API_NAME_CONF, ConfigDef.Type.STRING, CAMEL_SINK_BRAINTREE_PATH_API_NAME_DEFAULT, ConfigDef.Importance.HIGH, CAMEL_SINK_BRAINTREE_PATH_API_NAME_DOC);
-        conf.define(CAMEL_SINK_BRAINTREE_PATH_METHOD_NAME_CONF, ConfigDef.Type.STRING, CAMEL_SINK_BRAINTREE_PATH_METHOD_NAME_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SINK_BRAINTREE_PATH_METHOD_NAME_DOC);
+        conf.define(CAMEL_SINK_BRAINTREE_PATH_METHOD_NAME_CONF, ConfigDef.Type.STRING, CAMEL_SINK_BRAINTREE_PATH_METHOD_NAME_DEFAULT, ConfigDef.Importance.HIGH, CAMEL_SINK_BRAINTREE_PATH_METHOD_NAME_DOC);
         conf.define(CAMEL_SINK_BRAINTREE_ENDPOINT_ENVIRONMENT_CONF, ConfigDef.Type.STRING, CAMEL_SINK_BRAINTREE_ENDPOINT_ENVIRONMENT_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SINK_BRAINTREE_ENDPOINT_ENVIRONMENT_DOC);
         conf.define(CAMEL_SINK_BRAINTREE_ENDPOINT_IN_BODY_CONF, ConfigDef.Type.STRING, CAMEL_SINK_BRAINTREE_ENDPOINT_IN_BODY_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SINK_BRAINTREE_ENDPOINT_IN_BODY_DOC);
-        conf.define(CAMEL_SINK_BRAINTREE_ENDPOINT_MERCHANT_ID_CONF, ConfigDef.Type.STRING, CAMEL_SINK_BRAINTREE_ENDPOINT_MERCHANT_ID_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SINK_BRAINTREE_ENDPOINT_MERCHANT_ID_DOC);
         conf.define(CAMEL_SINK_BRAINTREE_ENDPOINT_LAZY_START_PRODUCER_CONF, ConfigDef.Type.BOOLEAN, CAMEL_SINK_BRAINTREE_ENDPOINT_LAZY_START_PRODUCER_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SINK_BRAINTREE_ENDPOINT_LAZY_START_PRODUCER_DOC);
+        conf.define(CAMEL_SINK_BRAINTREE_ENDPOINT_MERCHANT_ID_CONF, ConfigDef.Type.STRING, CAMEL_SINK_BRAINTREE_ENDPOINT_MERCHANT_ID_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SINK_BRAINTREE_ENDPOINT_MERCHANT_ID_DOC);
         conf.define(CAMEL_SINK_BRAINTREE_ENDPOINT_BASIC_PROPERTY_BINDING_CONF, ConfigDef.Type.BOOLEAN, CAMEL_SINK_BRAINTREE_ENDPOINT_BASIC_PROPERTY_BINDING_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SINK_BRAINTREE_ENDPOINT_BASIC_PROPERTY_BINDING_DOC);
         conf.define(CAMEL_SINK_BRAINTREE_ENDPOINT_HTTP_READ_TIMEOUT_CONF, ConfigDef.Type.STRING, CAMEL_SINK_BRAINTREE_ENDPOINT_HTTP_READ_TIMEOUT_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SINK_BRAINTREE_ENDPOINT_HTTP_READ_TIMEOUT_DOC);
         conf.define(CAMEL_SINK_BRAINTREE_ENDPOINT_SYNCHRONOUS_CONF, ConfigDef.Type.BOOLEAN, CAMEL_SINK_BRAINTREE_ENDPOINT_SYNCHRONOUS_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SINK_BRAINTREE_ENDPOINT_SYNCHRONOUS_DOC);
@@ -117,7 +117,7 @@ public class CamelBraintreeSinkConnectorConfig
         conf.define(CAMEL_SINK_BRAINTREE_ENDPOINT_PRIVATE_KEY_CONF, ConfigDef.Type.STRING, CAMEL_SINK_BRAINTREE_ENDPOINT_PRIVATE_KEY_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SINK_BRAINTREE_ENDPOINT_PRIVATE_KEY_DOC);
         conf.define(CAMEL_SINK_BRAINTREE_ENDPOINT_PUBLIC_KEY_CONF, ConfigDef.Type.STRING, CAMEL_SINK_BRAINTREE_ENDPOINT_PUBLIC_KEY_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SINK_BRAINTREE_ENDPOINT_PUBLIC_KEY_DOC);
         conf.define(CAMEL_SINK_BRAINTREE_COMPONENT_LAZY_START_PRODUCER_CONF, ConfigDef.Type.BOOLEAN, CAMEL_SINK_BRAINTREE_COMPONENT_LAZY_START_PRODUCER_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SINK_BRAINTREE_COMPONENT_LAZY_START_PRODUCER_DOC);
-        conf.define(CAMEL_SINK_BRAINTREE_COMPONENT_BASIC_PROPERTY_BINDING_CONF, ConfigDef.Type.BOOLEAN, CAMEL_SINK_BRAINTREE_COMPONENT_BASIC_PROPERTY_BINDING_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SINK_BRAINTREE_COMPONENT_BASIC_PROPERTY_BINDING_DOC);
+        conf.define(CAMEL_SINK_BRAINTREE_COMPONENT_BASIC_PROPERTY_BINDING_CONF, ConfigDef.Type.BOOLEAN, CAMEL_SINK_BRAINTREE_COMPONENT_BASIC_PROPERTY_BINDING_DEFAULT, ConfigDef.Importance.LOW, CAMEL_SINK_BRAINTREE_COMPONENT_BASIC_PROPERTY_BINDING_DOC);
         conf.define(CAMEL_SINK_BRAINTREE_COMPONENT_CONFIGURATION_CONF, ConfigDef.Type.STRING, CAMEL_SINK_BRAINTREE_COMPONENT_CONFIGURATION_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SINK_BRAINTREE_COMPONENT_CONFIGURATION_DOC);
         return conf;
     }
