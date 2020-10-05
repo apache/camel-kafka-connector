8cf27cb73941726e99497efc518da5d050b33569
Andrea Cosentino <ancosen@gmail.com>
2020-10-05 11:27:31 +0200

    Fixed CS

--- core/src/main/java/org/apache/camel/kafkaconnector/utils/CamelMainSupport.java
+++ core/src/main/java/org/apache/camel/kafkaconnector/utils/CamelMainSupport.java
@@ -17,7 +17,6 @@
 package org.apache.camel.kafkaconnector.utils;
 
 import java.util.Collection;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
