ed63f4374e9045be004d652ff33943041fa6042d
Andrea Cosentino <ancosen@gmail.com>
2020-10-06 19:01:06 +0200

    Move model classes CamelKafkaConnectorModel and CamelKafkaConnectorOptionModel in their own module

--- tooling/camel-kafka-connector-model/src/main/java/org/apache/camel/kafkaconnector/model/CamelKafkaConnectorOptionModel.java
+++ tooling/camel-kafka-connector-model/src/main/java/org/apache/camel/kafkaconnector/model/CamelKafkaConnectorOptionModel.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.camel.kafkaconnector.maven.dto;
+package org.apache.camel.kafkaconnector.model;
 
 import org.apache.camel.tooling.model.Strings;
 
