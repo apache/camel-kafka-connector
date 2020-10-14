7af8931f617084e0dc0c33038c786026f2fb4922
Andrea Cosentino <ancosen@gmail.com>
2020-10-14 11:45:42 +0200

    Rename all transformers, converters, aggregationStrategies to reflect the maning convention we have - aws2-sqs

--- connectors/camel-aws2-sqs-kafka-connector/src/main/java/org/apache/camel/kafkaconnector/aws2sqs/transformers/SQSKeySetterTransforms.java
+++ connectors/camel-aws2-sqs-kafka-connector/src/main/java/org/apache/camel/kafkaconnector/aws2sqs/transformers/SQSKeySetterTransforms.java
@@ -24,7 +24,7 @@ import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.header.Headers;
 import org.apache.kafka.connect.transforms.Transformation;
 
-public class SQSKeySetterTransformers<R extends ConnectRecord<R>> implements Transformation<R> {
+public class SQSKeySetterTransforms<R extends ConnectRecord<R>> implements Transformation<R> {
 
     public static final ConfigDef CONFIG_DEF = new ConfigDef()
             .define("test", ConfigDef.Type.STRING, "test", ConfigDef.Importance.MEDIUM, "Fetch the Camel.CamelAwsSqsMessageId header and set it as the key for the kafka record");
