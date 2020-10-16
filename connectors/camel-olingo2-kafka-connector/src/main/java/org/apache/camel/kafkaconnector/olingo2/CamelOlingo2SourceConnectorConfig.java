7b951c104ed6267bdf6004f8e29d23e501e70101
Andrea Cosentino <ancosen@gmail.com>
2020-10-16 07:22:27 +0200

    Regen for 3.4.4

--- connectors/camel-olingo2-kafka-connector/src/main/java/org/apache/camel/kafkaconnector/olingo2/CamelOlingo2SourceConnectorConfig.java
+++ connectors/camel-olingo2-kafka-connector/src/main/java/org/apache/camel/kafkaconnector/olingo2/CamelOlingo2SourceConnectorConfig.java
@@ -38,6 +38,12 @@ public class CamelOlingo2SourceConnectorConfig
     public static final String CAMEL_SOURCE_OLINGO2_ENDPOINT_CONTENT_TYPE_CONF = "camel.source.endpoint.contentType";
     public static final String CAMEL_SOURCE_OLINGO2_ENDPOINT_CONTENT_TYPE_DOC = "Content-Type header value can be used to specify JSON or XML message format, defaults to application/json;charset=utf-8";
     public static final String CAMEL_SOURCE_OLINGO2_ENDPOINT_CONTENT_TYPE_DEFAULT = "application/json;charset=utf-8";
+    public static final String CAMEL_SOURCE_OLINGO2_ENDPOINT_ENTITY_PROVIDER_READ_PROPERTIES_CONF = "camel.source.endpoint.entityProviderReadProperties";
+    public static final String CAMEL_SOURCE_OLINGO2_ENDPOINT_ENTITY_PROVIDER_READ_PROPERTIES_DOC = "Custom entity provider read properties applied to all read operations.";
+    public static final String CAMEL_SOURCE_OLINGO2_ENDPOINT_ENTITY_PROVIDER_READ_PROPERTIES_DEFAULT = null;
+    public static final String CAMEL_SOURCE_OLINGO2_ENDPOINT_ENTITY_PROVIDER_WRITE_PROPERTIES_CONF = "camel.source.endpoint.entityProviderWriteProperties";
+    public static final String CAMEL_SOURCE_OLINGO2_ENDPOINT_ENTITY_PROVIDER_WRITE_PROPERTIES_DOC = "Custom entity provider write properties applied to create, update, patch, batch and merge operations. For instance users can skip the Json object wrapper or enable content only mode when sending request data. A service URI set in the properties will always be overwritten by the serviceUri configuration parameter. Please consider to using the serviceUri configuration parameter instead of setting the respective write property here.";
+    public static final String CAMEL_SOURCE_OLINGO2_ENDPOINT_ENTITY_PROVIDER_WRITE_PROPERTIES_DEFAULT = null;
     public static final String CAMEL_SOURCE_OLINGO2_ENDPOINT_FILTER_ALREADY_SEEN_CONF = "camel.source.endpoint.filterAlreadySeen";
     public static final String CAMEL_SOURCE_OLINGO2_ENDPOINT_FILTER_ALREADY_SEEN_DOC = "Set this to true to filter out results that have already been communicated by this component.";
     public static final Boolean CAMEL_SOURCE_OLINGO2_ENDPOINT_FILTER_ALREADY_SEEN_DEFAULT = false;
@@ -140,6 +146,12 @@ public class CamelOlingo2SourceConnectorConfig
     public static final String CAMEL_SOURCE_OLINGO2_COMPONENT_CONTENT_TYPE_CONF = "camel.component.olingo2.contentType";
     public static final String CAMEL_SOURCE_OLINGO2_COMPONENT_CONTENT_TYPE_DOC = "Content-Type header value can be used to specify JSON or XML message format, defaults to application/json;charset=utf-8";
     public static final String CAMEL_SOURCE_OLINGO2_COMPONENT_CONTENT_TYPE_DEFAULT = "application/json;charset=utf-8";
+    public static final String CAMEL_SOURCE_OLINGO2_COMPONENT_ENTITY_PROVIDER_READ_PROPERTIES_CONF = "camel.component.olingo2.entityProviderReadProperties";
+    public static final String CAMEL_SOURCE_OLINGO2_COMPONENT_ENTITY_PROVIDER_READ_PROPERTIES_DOC = "Custom entity provider read properties applied to all read operations.";
+    public static final String CAMEL_SOURCE_OLINGO2_COMPONENT_ENTITY_PROVIDER_READ_PROPERTIES_DEFAULT = null;
+    public static final String CAMEL_SOURCE_OLINGO2_COMPONENT_ENTITY_PROVIDER_WRITE_PROPERTIES_CONF = "camel.component.olingo2.entityProviderWriteProperties";
+    public static final String CAMEL_SOURCE_OLINGO2_COMPONENT_ENTITY_PROVIDER_WRITE_PROPERTIES_DOC = "Custom entity provider write properties applied to create, update, patch, batch and merge operations. For instance users can skip the Json object wrapper or enable content only mode when sending request data. A service URI set in the properties will always be overwritten by the serviceUri configuration parameter. Please consider to using the serviceUri configuration parameter instead of setting the respective write property here.";
+    public static final String CAMEL_SOURCE_OLINGO2_COMPONENT_ENTITY_PROVIDER_WRITE_PROPERTIES_DEFAULT = null;
     public static final String CAMEL_SOURCE_OLINGO2_COMPONENT_FILTER_ALREADY_SEEN_CONF = "camel.component.olingo2.filterAlreadySeen";
     public static final String CAMEL_SOURCE_OLINGO2_COMPONENT_FILTER_ALREADY_SEEN_DOC = "Set this to true to filter out results that have already been communicated by this component.";
     public static final Boolean CAMEL_SOURCE_OLINGO2_COMPONENT_FILTER_ALREADY_SEEN_DEFAULT = false;
@@ -193,6 +205,8 @@ public class CamelOlingo2SourceConnectorConfig
         conf.define(CAMEL_SOURCE_OLINGO2_PATH_METHOD_NAME_CONF, ConfigDef.Type.STRING, CAMEL_SOURCE_OLINGO2_PATH_METHOD_NAME_DEFAULT, ConfigDef.Importance.HIGH, CAMEL_SOURCE_OLINGO2_PATH_METHOD_NAME_DOC);
         conf.define(CAMEL_SOURCE_OLINGO2_ENDPOINT_CONNECT_TIMEOUT_CONF, ConfigDef.Type.INT, CAMEL_SOURCE_OLINGO2_ENDPOINT_CONNECT_TIMEOUT_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SOURCE_OLINGO2_ENDPOINT_CONNECT_TIMEOUT_DOC);
         conf.define(CAMEL_SOURCE_OLINGO2_ENDPOINT_CONTENT_TYPE_CONF, ConfigDef.Type.STRING, CAMEL_SOURCE_OLINGO2_ENDPOINT_CONTENT_TYPE_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SOURCE_OLINGO2_ENDPOINT_CONTENT_TYPE_DOC);
+        conf.define(CAMEL_SOURCE_OLINGO2_ENDPOINT_ENTITY_PROVIDER_READ_PROPERTIES_CONF, ConfigDef.Type.STRING, CAMEL_SOURCE_OLINGO2_ENDPOINT_ENTITY_PROVIDER_READ_PROPERTIES_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SOURCE_OLINGO2_ENDPOINT_ENTITY_PROVIDER_READ_PROPERTIES_DOC);
+        conf.define(CAMEL_SOURCE_OLINGO2_ENDPOINT_ENTITY_PROVIDER_WRITE_PROPERTIES_CONF, ConfigDef.Type.STRING, CAMEL_SOURCE_OLINGO2_ENDPOINT_ENTITY_PROVIDER_WRITE_PROPERTIES_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SOURCE_OLINGO2_ENDPOINT_ENTITY_PROVIDER_WRITE_PROPERTIES_DOC);
         conf.define(CAMEL_SOURCE_OLINGO2_ENDPOINT_FILTER_ALREADY_SEEN_CONF, ConfigDef.Type.BOOLEAN, CAMEL_SOURCE_OLINGO2_ENDPOINT_FILTER_ALREADY_SEEN_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SOURCE_OLINGO2_ENDPOINT_FILTER_ALREADY_SEEN_DOC);
         conf.define(CAMEL_SOURCE_OLINGO2_ENDPOINT_HTTP_HEADERS_CONF, ConfigDef.Type.STRING, CAMEL_SOURCE_OLINGO2_ENDPOINT_HTTP_HEADERS_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SOURCE_OLINGO2_ENDPOINT_HTTP_HEADERS_DOC);
         conf.define(CAMEL_SOURCE_OLINGO2_ENDPOINT_IN_BODY_CONF, ConfigDef.Type.STRING, CAMEL_SOURCE_OLINGO2_ENDPOINT_IN_BODY_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SOURCE_OLINGO2_ENDPOINT_IN_BODY_DOC);
@@ -227,6 +241,8 @@ public class CamelOlingo2SourceConnectorConfig
         conf.define(CAMEL_SOURCE_OLINGO2_COMPONENT_CONFIGURATION_CONF, ConfigDef.Type.STRING, CAMEL_SOURCE_OLINGO2_COMPONENT_CONFIGURATION_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SOURCE_OLINGO2_COMPONENT_CONFIGURATION_DOC);
         conf.define(CAMEL_SOURCE_OLINGO2_COMPONENT_CONNECT_TIMEOUT_CONF, ConfigDef.Type.INT, CAMEL_SOURCE_OLINGO2_COMPONENT_CONNECT_TIMEOUT_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SOURCE_OLINGO2_COMPONENT_CONNECT_TIMEOUT_DOC);
         conf.define(CAMEL_SOURCE_OLINGO2_COMPONENT_CONTENT_TYPE_CONF, ConfigDef.Type.STRING, CAMEL_SOURCE_OLINGO2_COMPONENT_CONTENT_TYPE_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SOURCE_OLINGO2_COMPONENT_CONTENT_TYPE_DOC);
+        conf.define(CAMEL_SOURCE_OLINGO2_COMPONENT_ENTITY_PROVIDER_READ_PROPERTIES_CONF, ConfigDef.Type.STRING, CAMEL_SOURCE_OLINGO2_COMPONENT_ENTITY_PROVIDER_READ_PROPERTIES_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SOURCE_OLINGO2_COMPONENT_ENTITY_PROVIDER_READ_PROPERTIES_DOC);
+        conf.define(CAMEL_SOURCE_OLINGO2_COMPONENT_ENTITY_PROVIDER_WRITE_PROPERTIES_CONF, ConfigDef.Type.STRING, CAMEL_SOURCE_OLINGO2_COMPONENT_ENTITY_PROVIDER_WRITE_PROPERTIES_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SOURCE_OLINGO2_COMPONENT_ENTITY_PROVIDER_WRITE_PROPERTIES_DOC);
         conf.define(CAMEL_SOURCE_OLINGO2_COMPONENT_FILTER_ALREADY_SEEN_CONF, ConfigDef.Type.BOOLEAN, CAMEL_SOURCE_OLINGO2_COMPONENT_FILTER_ALREADY_SEEN_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SOURCE_OLINGO2_COMPONENT_FILTER_ALREADY_SEEN_DOC);
         conf.define(CAMEL_SOURCE_OLINGO2_COMPONENT_HTTP_HEADERS_CONF, ConfigDef.Type.STRING, CAMEL_SOURCE_OLINGO2_COMPONENT_HTTP_HEADERS_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SOURCE_OLINGO2_COMPONENT_HTTP_HEADERS_DOC);
         conf.define(CAMEL_SOURCE_OLINGO2_COMPONENT_PROXY_CONF, ConfigDef.Type.STRING, CAMEL_SOURCE_OLINGO2_COMPONENT_PROXY_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SOURCE_OLINGO2_COMPONENT_PROXY_DOC);
