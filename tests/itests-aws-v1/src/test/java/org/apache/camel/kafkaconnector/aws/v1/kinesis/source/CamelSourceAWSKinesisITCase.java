2bafb287ffc7974ec409f02b9072ebbb89812119
Otavio Rodolfo Piske <opiske@redhat.com>
2020-11-27 17:01:12 +0100

    Rework the AWS v1 tests to fix tests performance issues
    - Do not run unless enabled by slow tests flag
    - Prevent creating multiple instances of the service object
    - Do not instantiate the service object when disabled
    - Removes potentially slow annotation

--- tests/itests-aws-v1/src/test/java/org/apache/camel/kafkaconnector/aws/v1/kinesis/source/CamelSourceAWSKinesisITCase.java
+++ tests/itests-aws-v1/src/test/java/org/apache/camel/kafkaconnector/aws/v1/kinesis/source/CamelSourceAWSKinesisITCase.java
@@ -44,7 +44,9 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -52,16 +54,16 @@ import org.slf4j.LoggerFactory;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@EnabledIfSystemProperty(named = "enable.slow.tests", matches = "true")
 public class CamelSourceAWSKinesisITCase extends AbstractKafkaTest {
-    @RegisterExtension
-    public static AWSService<AmazonKinesis> service = AWSServiceFactory.createKinesisService();
-
     private static final Logger LOG = LoggerFactory.getLogger(CamelSourceAWSKinesisITCase.class);
 
+    @RegisterExtension
+    AWSService<AmazonKinesis> service = AWSServiceFactory.createKinesisService();
+
     private String streamName;
-
     private AmazonKinesis awsKinesisClient;
-
     private volatile int received;
     private final int expect = 10;
 
