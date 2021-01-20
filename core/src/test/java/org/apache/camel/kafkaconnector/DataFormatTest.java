b4f0f4ec259e44fd7b491a0c3171831a88d166ff
Andrea Cosentino <ancosen@gmail.com>
2021-01-20 07:28:36 +0100

    Fixed codestyle

--- core/src/test/java/org/apache/camel/kafkaconnector/DataFormatTest.java
+++ core/src/test/java/org/apache/camel/kafkaconnector/DataFormatTest.java
@@ -29,7 +29,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DataFormatTest {
 
