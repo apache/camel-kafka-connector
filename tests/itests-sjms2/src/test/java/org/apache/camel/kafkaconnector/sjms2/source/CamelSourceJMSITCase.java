e95b9fb3bd2f1874625fa81587a0400b52ce505f
Otavio Rodolfo Piske <opiske@redhat.com>
2021-02-08 21:21:28 +0100

    Converted the SJMS2 source test cases to use the reusable source base class

--- tests/itests-sjms2/src/test/java/org/apache/camel/kafkaconnector/sjms2/source/CamelSourceJMSITCase.java
+++ tests/itests-sjms2/src/test/java/org/apache/camel/kafkaconnector/sjms2/source/CamelSourceJMSITCase.java
@@ -20,26 +20,24 @@ package org.apache.camel.kafkaconnector.sjms2.source;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
-import org.apache.camel.kafkaconnector.common.AbstractKafkaTest;
 import org.apache.camel.kafkaconnector.common.ConnectorPropertyFactory;
 import org.apache.camel.kafkaconnector.common.clients.kafka.KafkaClient;
-import org.apache.camel.kafkaconnector.common.utils.TestUtils;
+import org.apache.camel.kafkaconnector.common.test.CamelSourceTestSupport;
+import org.apache.camel.kafkaconnector.common.test.IntegerMessageConsumer;
+import org.apache.camel.kafkaconnector.common.test.TestMessageConsumer;
 import org.apache.camel.kafkaconnector.sjms2.clients.JMSClient;
 import org.apache.camel.kafkaconnector.sjms2.common.SJMS2Common;
 import org.apache.camel.test.infra.dispatch.router.services.DispatchRouterContainer;
 import org.apache.camel.test.infra.messaging.services.MessagingService;
 import org.apache.camel.test.infra.messaging.services.MessagingServiceBuilder;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.api.extension.RegisterExtension;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 
 /**
@@ -47,16 +45,14 @@ import static org.junit.jupiter.api.Assertions.fail;
  * messages
  */
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class CamelSourceJMSITCase extends AbstractKafkaTest {
+public class CamelSourceJMSITCase extends CamelSourceTestSupport {
     @RegisterExtension
     public static MessagingService jmsService = MessagingServiceBuilder
             .newBuilder(DispatchRouterContainer::new)
             .withEndpointProvider(DispatchRouterContainer::defaultEndpoint)
             .build();
 
-    private static final Logger LOG = LoggerFactory.getLogger(CamelSourceJMSITCase.class);
-
-    private int received;
+    private String topicName;
     private final int expect = 10;
     private JMSClient jmsClient;
 
@@ -76,101 +72,68 @@ public class CamelSourceJMSITCase extends AbstractKafkaTest {
 
     @BeforeEach
     public void setUp() {
-        received = 0;
+        topicName = getTopicForTest(this);
+
+    }
+
+    @BeforeAll
+    public void setupClient() {
         jmsClient = JMSClient.newClient(jmsService.defaultEndpoint());
     }
 
-    private <T> boolean checkRecord(ConsumerRecord<String, T> record) {
-        LOG.debug("Received: {}", record.value());
-        received++;
-
-        if (received == expect) {
-            return false;
-        }
-
-        return true;
+    @Override
+    protected void produceTestData() {
+        JMSClient.produceMessages(jmsClient, SJMS2Common.DEFAULT_JMS_QUEUE, expect, "Test string message");
     }
 
-
-
-    public void runBasicStringTest(ConnectorPropertyFactory connectorPropertyFactory) throws ExecutionException, InterruptedException {
-        connectorPropertyFactory.log();
-        getKafkaConnectService().initializeConnector(connectorPropertyFactory);
-
-        JMSClient.produceMessages(jmsClient, SJMS2Common.DEFAULT_JMS_QUEUE, expect, "Test string message");
-
-        LOG.debug("Creating the consumer ...");
-        KafkaClient<String, String> kafkaClient = new KafkaClient<>(getKafkaService().getBootstrapServers());
-        kafkaClient.consume(TestUtils.getDefaultTestTopic(this.getClass()), this::checkRecord);
-        LOG.debug("Created the consumer ...");
-
+    @Override
+    protected void verifyMessages(TestMessageConsumer<?> consumer) {
+        int received = consumer.consumedMessages().size();
         assertEquals(received, expect, "Didn't process the expected amount of messages");
     }
 
+
     @Test
     @Timeout(90)
-    public void testBasicSendReceive() {
-        try {
-            ConnectorPropertyFactory connectorPropertyFactory = CamelJMSPropertyFactory
-                    .basic()
-                    .withKafkaTopic(TestUtils.getDefaultTestTopic(this.getClass()))
-                    .withDestinationName(SJMS2Common.DEFAULT_JMS_QUEUE)
-                    .withConnectionProperties(connectionProperties());
+    public void testBasicSendReceive() throws ExecutionException, InterruptedException {
+        ConnectorPropertyFactory connectorPropertyFactory = CamelJMSPropertyFactory
+                .basic()
+                .withKafkaTopic(topicName)
+                .withDestinationName(SJMS2Common.DEFAULT_JMS_QUEUE)
+                .withConnectionProperties(connectionProperties());
 
-            runBasicStringTest(connectorPropertyFactory);
-        } catch (Exception e) {
-            LOG.error("JMS test failed: {}", e.getMessage(), e);
-            fail(e.getMessage());
-        }
+        runTest(connectorPropertyFactory, topicName, expect);
     }
 
     @Test
     @Timeout(90)
-    public void testBasicSendReceiveUsingUrl() {
-        try {
-            ConnectorPropertyFactory connectorPropertyFactory = CamelJMSPropertyFactory
-                    .basic()
-                    .withConnectionProperties(connectionProperties())
-                    .withKafkaTopic(TestUtils.getDefaultTestTopic(this.getClass()))
-                    .withUrl(SJMS2Common.DEFAULT_JMS_QUEUE)
-                        .buildUrl();
+    public void testBasicSendReceiveUsingUrl() throws ExecutionException, InterruptedException {
+        ConnectorPropertyFactory connectorPropertyFactory = CamelJMSPropertyFactory
+                .basic()
+                .withConnectionProperties(connectionProperties())
+                .withKafkaTopic(topicName)
+                .withUrl(SJMS2Common.DEFAULT_JMS_QUEUE)
+                    .buildUrl();
 
-            runBasicStringTest(connectorPropertyFactory);
-        } catch (Exception e) {
-            LOG.error("JMS test failed: {}", e.getMessage(), e);
-            fail(e.getMessage());
-        }
+        runTest(connectorPropertyFactory, topicName, expect);
     }
 
 
     @Test
     @Timeout(90)
-    public void testIntSendReceive() {
-        try {
-            final String jmsQueueName = "testIntSendReceive";
+    public void testIntSendReceive() throws ExecutionException, InterruptedException {
+        final String jmsQueueName = "testIntSendReceive";
 
-            ConnectorPropertyFactory connectorPropertyFactory = CamelJMSPropertyFactory
-                    .basic()
-                    .withKafkaTopic(TestUtils.getDefaultTestTopic(this.getClass()) + jmsQueueName)
-                    .withDestinationName(jmsQueueName)
-                    .withConnectionProperties(connectionProperties());
+        ConnectorPropertyFactory connectorPropertyFactory = CamelJMSPropertyFactory
+                .basic()
+                .withKafkaTopic(topicName)
+                .withDestinationName(jmsQueueName)
+                .withConnectionProperties(connectionProperties());
 
-            connectorPropertyFactory.log();
-            getKafkaConnectService().initializeConnector(connectorPropertyFactory);
+        KafkaClient<String, Integer> kafkaClient = new KafkaClient<>(getKafkaService().getBootstrapServers());
+        IntegerMessageConsumer consumer = new IntegerMessageConsumer(kafkaClient, topicName, expect);
 
-            JMSClient.produceMessages(jmsClient, jmsQueueName, expect);
-
-            LOG.debug("Creating the consumer ...");
-            KafkaClient<String, Integer> kafkaClient = new KafkaClient<>(getKafkaService().getBootstrapServers());
-            kafkaClient.consume(TestUtils.getDefaultTestTopic(this.getClass()) + "testIntSendReceive", this::checkRecord);
-            LOG.debug("Created the consumer ...");
-
-            assertEquals(received, expect, "Didn't process the expected amount of messages");
-        } catch (Exception e) {
-            LOG.error("JMS test failed: {}", e.getMessage(), e);
-            fail(e.getMessage());
-        }
-
+        runTest(connectorPropertyFactory, consumer, () -> JMSClient.produceMessages(jmsClient, jmsQueueName, expect));
     }
 
 
--- tests/itests-sjms2/src/test/java/org/apache/camel/kafkaconnector/sjms2/source/CamelSourceJMSWithAggregation.java
+++ tests/itests-sjms2/src/test/java/org/apache/camel/kafkaconnector/sjms2/source/CamelSourceJMSWithAggregation.java
@@ -20,44 +20,65 @@ package org.apache.camel.kafkaconnector.sjms2.source;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
-import org.apache.camel.kafkaconnector.common.AbstractKafkaTest;
 import org.apache.camel.kafkaconnector.common.ConnectorPropertyFactory;
 import org.apache.camel.kafkaconnector.common.clients.kafka.KafkaClient;
+import org.apache.camel.kafkaconnector.common.test.CamelSourceTestSupport;
+import org.apache.camel.kafkaconnector.common.test.StringMessageConsumer;
+import org.apache.camel.kafkaconnector.common.test.TestMessageConsumer;
 import org.apache.camel.kafkaconnector.common.utils.TestUtils;
 import org.apache.camel.kafkaconnector.sjms2.clients.JMSClient;
 import org.apache.camel.kafkaconnector.sjms2.common.SJMS2Common;
 import org.apache.camel.test.infra.dispatch.router.services.DispatchRouterContainer;
 import org.apache.camel.test.infra.messaging.services.MessagingService;
 import org.apache.camel.test.infra.messaging.services.MessagingServiceBuilder;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.api.extension.RegisterExtension;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class CamelSourceJMSWithAggregation extends AbstractKafkaTest {
+public class CamelSourceJMSWithAggregation extends CamelSourceTestSupport {
     @RegisterExtension
     public static MessagingService jmsService = MessagingServiceBuilder
             .newBuilder(DispatchRouterContainer::new)
             .withEndpointProvider(DispatchRouterContainer::defaultEndpoint)
             .build();
 
-    private static final Logger LOG = LoggerFactory.getLogger(CamelSourceJMSITCase.class);
-
-    private int received;
     private final int sentSize = 10;
     private final int expect = 1;
     private JMSClient jmsClient;
-    private String receivedMessage = "";
     private String expectedMessage = "";
     private String queueName;
+    private String topicName;
+
+    class GreedyConsumer extends StringMessageConsumer {
+
+        public GreedyConsumer(KafkaClient<String, String> kafkaClient, String topicName, int count) {
+            super(kafkaClient, topicName, count);
+        }
+
+        @Override
+        public void consumeMessages() {
+            int retries = 10;
+
+            do {
+                kafkaClient.consumeAvailable(super.topicName, super::checkRecord);
+                if (consumedMessages().size() == 0) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        break;
+                    }
+                }
+
+            } while (consumedMessages().size() == 0);
+        }
+    }
 
     private Properties connectionProperties() {
         Properties properties = new Properties();
@@ -73,9 +94,8 @@ public class CamelSourceJMSWithAggregation extends AbstractKafkaTest {
         return new String[] {"camel-sjms2-kafka-connector"};
     }
 
-    @BeforeEach
-    public void setUp() {
-        received = 0;
+    @BeforeAll
+    public void setupClient() {
         jmsClient = JMSClient.newClient(jmsService.defaultEndpoint());
 
         for (int i = 0; i < sentSize - 1; i++) {
@@ -83,53 +103,55 @@ public class CamelSourceJMSWithAggregation extends AbstractKafkaTest {
         }
 
         expectedMessage += "hello;";
+    }
+
+    @BeforeEach
+    public void setUp() {
+        topicName = getTopicForTest(this);
+
         queueName = SJMS2Common.DEFAULT_JMS_QUEUE + "." + TestUtils.randomWithRange(1, 100);
     }
 
-    private void checkRecord(ConsumerRecord<String, String> record) {
-        receivedMessage += record.value();
-        LOG.debug("Received: {}", receivedMessage);
-
-        received++;
-    }
-
-    private static String textToSend(Integer i) {
-        return "hello;";
-    }
-
-
-    public void runBasicStringTest(ConnectorPropertyFactory connectorPropertyFactory) throws ExecutionException, InterruptedException {
-        connectorPropertyFactory.log();
-        getKafkaConnectService().initializeConnectorBlocking(connectorPropertyFactory, 1);
-
+    @Override
+    protected void produceTestData() {
         JMSClient.produceMessages(jmsClient, queueName, sentSize,
                 CamelSourceJMSWithAggregation::textToSend);
+    }
 
-        LOG.debug("Creating the consumer ...");
-        KafkaClient<String, String> kafkaClient = new KafkaClient<>(getKafkaService().getBootstrapServers());
-        kafkaClient.consumeAvailable(TestUtils.getDefaultTestTopic(this.getClass()), this::checkRecord);
-        LOG.debug("Created the consumer ...");
+    @Override
+    protected void verifyMessages(TestMessageConsumer<?> consumer) {
+        int received = consumer.consumedMessages().size();
+
+        Object receivedObject = consumer.consumedMessages().get(0).value();
+        if (!(receivedObject instanceof String)) {
+            fail("Unexpected message type");
+        }
+
+        String receivedMessage = (String) receivedObject;
 
         assertEquals(expect, received, "Didn't process the expected amount of messages");
         assertEquals(expectedMessage, receivedMessage, "The messages don't match");
     }
 
+
+    private static String textToSend(Integer i) {
+        return "hello;";
+    }
+
     @Test
     @Timeout(90)
-    public void testBasicSendReceive() {
-        try {
-            ConnectorPropertyFactory connectorPropertyFactory = CamelJMSPropertyFactory
-                    .basic()
-                    .withKafkaTopic(TestUtils.getDefaultTestTopic(this.getClass()))
-                    .withDestinationName(queueName)
-                    .withConnectionProperties(connectionProperties())
-                    .withAggregate("org.apache.camel.kafkaconnector.aggregator.StringAggregator", sentSize,
-                            1000);
+    public void testBasicSendReceive() throws ExecutionException, InterruptedException {
+        ConnectorPropertyFactory connectorPropertyFactory = CamelJMSPropertyFactory
+                .basic()
+                .withKafkaTopic(topicName)
+                .withDestinationName(queueName)
+                .withConnectionProperties(connectionProperties())
+                .withAggregate("org.apache.camel.kafkaconnector.aggregator.StringAggregator", sentSize,
+                        1000);
 
-            runBasicStringTest(connectorPropertyFactory);
-        } catch (Exception e) {
-            LOG.error("JMS test failed: {}", e.getMessage(), e);
-            fail(e.getMessage());
-        }
+        KafkaClient<String, String> kafkaClient = new KafkaClient<>(getKafkaService().getBootstrapServers());
+        GreedyConsumer greedyConsumer = new GreedyConsumer(kafkaClient, topicName, expect);
+
+        runTestBlocking(connectorPropertyFactory, greedyConsumer);
     }
 }
