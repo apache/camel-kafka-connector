b7ead72de888511ae3df7129fa6c26cdfc793d70
Otavio Rodolfo Piske <opiske@redhat.com>
2021-02-19 12:07:54 +0100

    Abstracted the CXF service client setup from the CXF test code

--- tests/itests-cxf/src/test/java/org/apache/camel/kafkaconnector/cxf/source/CamelSourceCXFITCase.java
+++ tests/itests-cxf/src/test/java/org/apache/camel/kafkaconnector/cxf/source/CamelSourceCXFITCase.java
@@ -24,12 +24,7 @@ import org.apache.camel.kafkaconnector.common.ConnectorPropertyFactory;
 import org.apache.camel.kafkaconnector.common.clients.kafka.KafkaClient;
 import org.apache.camel.kafkaconnector.common.utils.NetworkUtils;
 import org.apache.camel.kafkaconnector.common.utils.TestUtils;
-import org.apache.cxf.Bus;
-import org.apache.cxf.BusFactory;
-import org.apache.cxf.ext.logging.LoggingInInterceptor;
-import org.apache.cxf.ext.logging.LoggingOutInterceptor;
-import org.apache.cxf.frontend.ClientFactoryBean;
-import org.apache.cxf.frontend.ClientProxyFactoryBean;
+import org.apache.camel.kafkaconnector.cxf.client.CXFServiceUtil;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -66,7 +61,6 @@ public class CamelSourceCXFITCase extends AbstractKafkaTest {
     @BeforeEach
     public void setUp() {
         received = 0;
-
     }
 
     private <T> boolean checkRecord(ConsumerRecord<String, T> record) {
@@ -88,15 +82,7 @@ public class CamelSourceCXFITCase extends AbstractKafkaTest {
 
         // ensure cxf source connector is up
         Thread.sleep(5000);
-        ClientProxyFactoryBean proxyFactory = new ClientProxyFactoryBean();
-        ClientFactoryBean clientBean = proxyFactory.getClientFactoryBean();
-        clientBean.setAddress(SIMPLE_ENDPOINT_ADDRESS);
-        clientBean.setServiceClass(HelloService.class);
-        Bus bus = BusFactory.newInstance().createBus();
-        clientBean.setBus(bus);
-        bus.getInInterceptors().add(new LoggingInInterceptor());
-        bus.getOutInterceptors().add(new LoggingOutInterceptor());
-        HelloService client = (HelloService) proxyFactory.create();
+        HelloService client = CXFServiceUtil.getService(SIMPLE_ENDPOINT_ADDRESS, HelloService.class);
         try {
             String result = client.echo(TEST_MESSAGE);
             assertEquals(result, TEST_MESSAGE);
