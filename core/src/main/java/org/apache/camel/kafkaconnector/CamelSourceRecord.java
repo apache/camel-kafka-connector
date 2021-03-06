c96272548b81612944725f553e73a868a8712cb2
Andrea Tarocchi <andrea.tarocchi@gmail.com>
2021-03-06 15:46:21 +0100

    chore: fix checkstyle.

--- core/src/main/java/org/apache/camel/kafkaconnector/CamelSourceRecord.java
+++ core/src/main/java/org/apache/camel/kafkaconnector/CamelSourceRecord.java
@@ -1,13 +1,29 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.camel.kafkaconnector;
+
+import java.util.Map;
 
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.header.Header;
 import org.apache.kafka.connect.source.SourceRecord;
 
-import java.util.Map;
-
 public class CamelSourceRecord extends SourceRecord {
-    private Integer claimCheck = null;
+    private Integer claimCheck;
 
     public CamelSourceRecord(Map<String, ?> sourcePartition, Map<String, ?> sourceOffset, String topic, Integer partition, Schema valueSchema, Object value) {
         super(sourcePartition, sourceOffset, topic, partition, valueSchema, value);
