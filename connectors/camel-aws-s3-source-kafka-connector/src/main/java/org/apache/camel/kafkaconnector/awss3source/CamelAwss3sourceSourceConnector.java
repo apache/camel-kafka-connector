48745335c6d8025dfe3c1e598c6a2e3d42d2d6eb
Andrea Cosentino <ancosen@gmail.com>
2022-08-22 15:55:05 +0200

    Regen with Kamelets-catalog 0.9.0

--- connectors/camel-aws-s3-source-kafka-connector/src/main/java/org/apache/camel/kafkaconnector/awss3source/CamelAwss3sourceSourceConnector.java
+++ connectors/camel-aws-s3-source-kafka-connector/src/main/java/org/apache/camel/kafkaconnector/awss3source/CamelAwss3sourceSourceConnector.java
@@ -13,8 +13,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
-package org.apache.camel.kafkaconnector.awss3source;
+ */package org.apache.camel.kafkaconnector.awss3source;
 
 import javax.annotation.Generated;
 import org.apache.camel.kafkaconnector.CamelSourceConnector;
