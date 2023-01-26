9e0cf301e62dd6896bb5044cabc06c9047d1a171
Andrea Tarocchi <andrea.tarocchi@gmail.com>
2023-01-26 16:40:04 +0100

    chore: fixed formatting.

--- connectors/camel-beer-source-kafka-connector/src/main/java/org/apache/camel/kafkaconnector/beersource/CamelBeersourceSourceConnectorConfig.java
+++ connectors/camel-beer-source-kafka-connector/src/main/java/org/apache/camel/kafkaconnector/beersource/CamelBeersourceSourceConnectorConfig.java
@@ -13,7 +13,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */package org.apache.camel.kafkaconnector.beersource;
+ */
+package org.apache.camel.kafkaconnector.beersource;
 
 import java.util.Map;
 import javax.annotation.Generated;
