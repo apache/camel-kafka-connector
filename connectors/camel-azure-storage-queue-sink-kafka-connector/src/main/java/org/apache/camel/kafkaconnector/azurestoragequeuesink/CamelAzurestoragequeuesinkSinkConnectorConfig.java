10741417f318dd63e809618536650f926cb2d15c
Andrea Tarocchi <andrea.tarocchi@gmail.com>
2023-05-03 12:21:16 +0200

    After release regen

--- connectors/camel-azure-storage-queue-sink-kafka-connector/src/main/java/org/apache/camel/kafkaconnector/azurestoragequeuesink/CamelAzurestoragequeuesinkSinkConnectorConfig.java
+++ connectors/camel-azure-storage-queue-sink-kafka-connector/src/main/java/org/apache/camel/kafkaconnector/azurestoragequeuesink/CamelAzurestoragequeuesinkSinkConnectorConfig.java
@@ -13,8 +13,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
-package org.apache.camel.kafkaconnector.azurestoragequeuesink;
+ */package org.apache.camel.kafkaconnector.azurestoragequeuesink;
 
 import java.util.Map;
 import javax.annotation.Generated;
