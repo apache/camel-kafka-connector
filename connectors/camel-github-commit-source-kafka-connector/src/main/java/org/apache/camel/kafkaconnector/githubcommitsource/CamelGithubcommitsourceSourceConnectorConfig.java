b64307b8da67a4047d117f7bac39c4b2b16e767f
Claus Ibsen <claus.ibsen@gmail.com>
2024-05-06 18:08:09 +0200

    Regen

--- connectors/camel-github-commit-source-kafka-connector/src/main/java/org/apache/camel/kafkaconnector/githubcommitsource/CamelGithubcommitsourceSourceConnectorConfig.java
+++ connectors/camel-github-commit-source-kafka-connector/src/main/java/org/apache/camel/kafkaconnector/githubcommitsource/CamelGithubcommitsourceSourceConnectorConfig.java
@@ -13,7 +13,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */package org.apache.camel.kafkaconnector.githubcommitsource;
+ */
+package org.apache.camel.kafkaconnector.githubcommitsource;
 
 import java.util.Map;
 import javax.annotation.Generated;
--- connectors/camel-jms-ibm-mq-sink-kafka-connector/src/main/java/org/apache/camel/kafkaconnector/jmsibmmqsink/CamelJmsibmmqsinkSinkTask.java
+++ connectors/camel-jms-ibm-mq-sink-kafka-connector/src/main/java/org/apache/camel/kafkaconnector/jmsibmmqsink/CamelJmsibmmqsinkSinkTask.java
@@ -13,7 +13,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */package org.apache.camel.kafkaconnector.jmsibmmqsink;
+ */
+package org.apache.camel.kafkaconnector.jmsibmmqsink;
 
 import java.util.HashMap;
 import java.util.Map;
