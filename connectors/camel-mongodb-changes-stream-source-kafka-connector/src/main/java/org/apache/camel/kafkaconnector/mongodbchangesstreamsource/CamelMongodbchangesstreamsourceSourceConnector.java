21eda4793ef00b0d9131eea60ded63474097e26b
Claus Ibsen <claus.ibsen@gmail.com>
2024-05-06 20:21:31 +0200

    Regen

--- connectors/camel-mongodb-changes-stream-source-kafka-connector/src/main/java/org/apache/camel/kafkaconnector/mongodbchangesstreamsource/CamelMongodbchangesstreamsourceSourceConnector.java
+++ connectors/camel-mongodb-changes-stream-source-kafka-connector/src/main/java/org/apache/camel/kafkaconnector/mongodbchangesstreamsource/CamelMongodbchangesstreamsourceSourceConnector.java
@@ -13,7 +13,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */package org.apache.camel.kafkaconnector.mongodbchangesstreamsource;
+ */
+package org.apache.camel.kafkaconnector.mongodbchangesstreamsource;
 
 import javax.annotation.Generated;
 import org.apache.camel.kafkaconnector.CamelSourceConnector;
