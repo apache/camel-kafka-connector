ffc25dfd4d6abfc3faf69ac0af2f3b5874a797d7
oscerd <5106647+oscerd@users.noreply.github.com>
2025-03-07 02:50:28 +0000

    [create-pull-request] automated change

--- connectors/camel-kafka-batch-ssl-source-kafka-connector/src/main/java/org/apache/camel/kafkaconnector/kafkabatchsslsource/CamelKafkabatchsslsourceSourceConnectorConfig.java
+++ connectors/camel-kafka-batch-ssl-source-kafka-connector/src/main/java/org/apache/camel/kafkaconnector/kafkabatchsslsource/CamelKafkabatchsslsourceSourceConnectorConfig.java
@@ -89,6 +89,9 @@ public class CamelKafkabatchsslsourceSourceConnectorConfig
     public static final String CAMEL_SOURCE_KAFKABATCHSSLSOURCE_KAMELET_MAX_POLL_INTERVAL_MS_CONF = "camel.kamelet.kafka-batch-ssl-source.maxPollIntervalMs";
     public static final String CAMEL_SOURCE_KAFKABATCHSSLSOURCE_KAMELET_MAX_POLL_INTERVAL_MS_DOC = "The maximum delay between invocations of poll() when using consumer group management";
     public static final Integer CAMEL_SOURCE_KAFKABATCHSSLSOURCE_KAMELET_MAX_POLL_INTERVAL_MS_DEFAULT = null;
+    public static final String CAMEL_SOURCE_KAFKABATCHSSLSOURCE_KAMELET_BATCHING_INTERVAL_MS_CONF = "camel.kamelet.kafka-batch-ssl-source.batchingIntervalMs";
+    public static final String CAMEL_SOURCE_KAFKABATCHSSLSOURCE_KAMELET_BATCHING_INTERVAL_MS_DOC = "In consumer batching mode, then this option is specifying a time in millis, to trigger batch completion eager when the current batch size has not reached the maximum size defined by maxPollRecords. Notice the trigger is not exact at the given interval, as this can only happen between kafka polls (see pollTimeoutMs option).";
+    public static final Integer CAMEL_SOURCE_KAFKABATCHSSLSOURCE_KAMELET_BATCHING_INTERVAL_MS_DEFAULT = null;
     public static final String CAMEL_SOURCE_KAFKABATCHSSLSOURCE_KAMELET_TOPIC_IS_PATTERN_CONF = "camel.kamelet.kafka-batch-ssl-source.topicIsPattern";
     public static final String CAMEL_SOURCE_KAFKABATCHSSLSOURCE_KAMELET_TOPIC_IS_PATTERN_DOC = "Whether the topic is a pattern (regular expression). This can be used to subscribe to dynamic number of topics matching the pattern.";
     public static final Boolean CAMEL_SOURCE_KAFKABATCHSSLSOURCE_KAMELET_TOPIC_IS_PATTERN_DEFAULT = false;
@@ -127,6 +130,7 @@ public class CamelKafkabatchsslsourceSourceConnectorConfig
         conf.define(CAMEL_SOURCE_KAFKABATCHSSLSOURCE_KAMELET_BATCH_SIZE_CONF, ConfigDef.Type.INT, CAMEL_SOURCE_KAFKABATCHSSLSOURCE_KAMELET_BATCH_SIZE_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SOURCE_KAFKABATCHSSLSOURCE_KAMELET_BATCH_SIZE_DOC);
         conf.define(CAMEL_SOURCE_KAFKABATCHSSLSOURCE_KAMELET_POLL_TIMEOUT_CONF, ConfigDef.Type.INT, CAMEL_SOURCE_KAFKABATCHSSLSOURCE_KAMELET_POLL_TIMEOUT_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SOURCE_KAFKABATCHSSLSOURCE_KAMELET_POLL_TIMEOUT_DOC);
         conf.define(CAMEL_SOURCE_KAFKABATCHSSLSOURCE_KAMELET_MAX_POLL_INTERVAL_MS_CONF, ConfigDef.Type.INT, CAMEL_SOURCE_KAFKABATCHSSLSOURCE_KAMELET_MAX_POLL_INTERVAL_MS_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SOURCE_KAFKABATCHSSLSOURCE_KAMELET_MAX_POLL_INTERVAL_MS_DOC);
+        conf.define(CAMEL_SOURCE_KAFKABATCHSSLSOURCE_KAMELET_BATCHING_INTERVAL_MS_CONF, ConfigDef.Type.INT, CAMEL_SOURCE_KAFKABATCHSSLSOURCE_KAMELET_BATCHING_INTERVAL_MS_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SOURCE_KAFKABATCHSSLSOURCE_KAMELET_BATCHING_INTERVAL_MS_DOC);
         conf.define(CAMEL_SOURCE_KAFKABATCHSSLSOURCE_KAMELET_TOPIC_IS_PATTERN_CONF, ConfigDef.Type.BOOLEAN, CAMEL_SOURCE_KAFKABATCHSSLSOURCE_KAMELET_TOPIC_IS_PATTERN_DEFAULT, ConfigDef.Importance.MEDIUM, CAMEL_SOURCE_KAFKABATCHSSLSOURCE_KAMELET_TOPIC_IS_PATTERN_DOC);
         return conf;
     }
