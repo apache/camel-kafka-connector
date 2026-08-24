apache/camel-kafka-connector

commit ca4e7843529d957e496d2eec82dce0ff2128aba6

Related to #423 modularized kamelets and composed them to better autogenerate connectors from kamelets catalog

core/src/main/java/org/apache/camel/kafkaconnector/utils/CamelKafkaConnectMain.java
@@ -22,7 +22,6 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.stream.Collectors;
 
-import org.apache.camel.AggregationStrategy;
 import org.apache.camel.CamelContext;
 import org.apache.camel.ConsumerTemplate;
 import org.apache.camel.ProducerTemplate;
@@ -31,11 +30,8 @@ import org.apache.camel.builder.ErrorHandlerBuilderRef;
 import org.apache.camel.builder.NoErrorHandlerBuilder;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.kafkaconnector.CamelConnectorConfig;
-import org.apache.camel.kafkaconnector.CamelSinkTask;
-import org.apache.camel.kafkaconnector.CamelSourceTask;
 import org.apache.camel.main.SimpleMain;
 import org.apache.camel.model.ProcessorDefinition;
-import org.apache.camel.model.RouteTemplateDefinition;
 import org.apache.camel.processor.idempotent.kafka.KafkaIdempotentRepository;
 import org.apache.camel.spi.IdempotentRepository;
 import org.apache.camel.support.processor.idempotent.MemoryIdempotentRepository;
@@ -46,6 +42,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CamelKafkaConnectMain extends SimpleMain {
+    public static final String KAMELET_MARSHAL_TEMPLATE_PARAMETERS_PREFIX = "camel.kamelet.ckcMarshal.";
+    public static final String KAMELET_UNMARSHAL_TEMPLATE_PARAMETERS_PREFIX = "camel.kamelet.ckcUnMarshal.";
+    public static final String KAMELET_AGGREGATORL_TEMPLATE_PARAMETERS_PREFIX = "camel.kamelet.ckcAggregator.";
+    public static final String KAMELET_IDEMPOTENT_TEMPLATE_PARAMETERS_PREFIX = "camel.kamelet.ckcIdempotent.";
+    public static final String KAMELET_REMOVEHEADER_TEMPLATE_PARAMETERS_PREFIX = "camel.kamelet.ckcRemoveHeader.";
+
     private static final Logger LOG = LoggerFactory.getLogger(CamelKafkaConnectMain.class);
 
     protected volatile ConsumerTemplate consumerTemplate;
@@ -228,36 +230,45 @@ public class CamelKafkaConnectMain extends SimpleMain {
             Properties camelProperties = new Properties();
             camelProperties.putAll(props);
 
+            //error handler
+            camelMain.getCamelContext().getRegistry().bind("ckcErrorHandler", new DefaultErrorHandlerBuilder());
+            if (errorHandler != null) {
+                switch (errorHandler) {
+                    case "no":
+                        camelMain.getCamelContext().getRegistry().bind("ckcErrorHandler", new NoErrorHandlerBuilder());
+                        break;
+                    case "default":
+                        camelMain.getCamelContext().getRegistry().bind("ckcErrorHandler", new DefaultErrorHandlerBuilder().maximumRedeliveries(maxRedeliveries).redeliveryDelay(redeliveryDelay));
+                        break;
+                    default:
+                        break;
+                }
+            }
+
             //dataformats
             if (!ObjectHelper.isEmpty(marshallDataFormat)) {
-                camelProperties.put(CamelSourceTask.KAMELET_SOURCE_TEMPLATE_PARAMETERS_PREFIX + "marshal", marshallDataFormat);
-                camelProperties.put(CamelSinkTask.KAMELET_SINK_TEMPLATE_PARAMETERS_PREFIX + "marshal", marshallDataFormat);
+                camelProperties.put(KAMELET_MARSHAL_TEMPLATE_PARAMETERS_PREFIX + "marshal", marshallDataFormat);
             }
             if (!ObjectHelper.isEmpty(unmarshallDataFormat)) {
-                camelProperties.put(CamelSourceTask.KAMELET_SOURCE_TEMPLATE_PARAMETERS_PREFIX + "unmarshal", unmarshallDataFormat);
-                camelProperties.put(CamelSinkTask.KAMELET_SINK_TEMPLATE_PARAMETERS_PREFIX + "unmarshal", unmarshallDataFormat);
+                camelProperties.put(KAMELET_UNMARSHAL_TEMPLATE_PARAMETERS_PREFIX + "unmarshal", unmarshallDataFormat);
             }
 
             //aggregator
             if (!ObjectHelper.isEmpty(aggregationSize)) {
-                camelProperties.put(CamelSourceTask.KAMELET_SOURCE_TEMPLATE_PARAMETERS_PREFIX + "aggregationSize", String.valueOf(aggregationSize));
-                camelProperties.put(CamelSinkTask.KAMELET_SINK_TEMPLATE_PARAMETERS_PREFIX + "aggregationSize", String.valueOf(aggregationSize));
+                camelProperties.put(KAMELET_AGGREGATORL_TEMPLATE_PARAMETERS_PREFIX + "aggregationSize", String.valueOf(aggregationSize));
             }
             if (!ObjectHelper.isEmpty(aggregationTimeout)) {
-                camelProperties.put(CamelSourceTask.KAMELET_SOURCE_TEMPLATE_PARAMETERS_PREFIX + "aggregationTimeout", String.valueOf(aggregationTimeout));
-                camelProperties.put(CamelSinkTask.KAMELET_SINK_TEMPLATE_PARAMETERS_PREFIX + "aggregationTimeout", String.valueOf(aggregationTimeout));
+                camelProperties.put(KAMELET_AGGREGATORL_TEMPLATE_PARAMETERS_PREFIX + "aggregationTimeout", String.valueOf(aggregationTimeout));
             }
 
             //idempotency
             if (idempotencyEnabled) {
                 switch (expressionType) {
                     case "body":
-                        camelProperties.put(CamelSourceTask.KAMELET_SOURCE_TEMPLATE_PARAMETERS_PREFIX + "idempotentExpression", "${body}");
-                        camelProperties.put(CamelSinkTask.KAMELET_SINK_TEMPLATE_PARAMETERS_PREFIX + "idempotentExpression", "${body}");
+                        camelProperties.put(KAMELET_IDEMPOTENT_TEMPLATE_PARAMETERS_PREFIX + "idempotentExpression", "${body}");
                         break;
                     case "header":
-                        camelProperties.put(CamelSourceTask.KAMELET_SOURCE_TEMPLATE_PARAMETERS_PREFIX + "idempotentExpression", "${headers." + expressionHeader + "}");
-                        camelProperties.put(CamelSinkTask.KAMELET_SINK_TEMPLATE_PARAMETERS_PREFIX + "idempotentExpression", "${headers." + expressionHeader + "}");
+                        camelProperties.put(KAMELET_IDEMPOTENT_TEMPLATE_PARAMETERS_PREFIX + "idempotentExpression", "${headers." + expressionHeader + "}");
                         break;
                     default:
                         break;
@@ -279,117 +290,92 @@ public class CamelKafkaConnectMain extends SimpleMain {
 
             //remove headers
             if (!ObjectHelper.isEmpty(headersExcludePattern)) {
-                camelProperties.put(CamelSourceTask.KAMELET_SOURCE_TEMPLATE_PARAMETERS_PREFIX + "headersExcludePattern", headersExcludePattern);
-                camelProperties.put(CamelSinkTask.KAMELET_SINK_TEMPLATE_PARAMETERS_PREFIX + "headersExcludePattern", headersExcludePattern);
+                camelProperties.put(KAMELET_REMOVEHEADER_TEMPLATE_PARAMETERS_PREFIX + "headersExcludePattern", headersExcludePattern);
             }
 
+            // log filtered properties and set initial camel properties
             List<String> filteredProps = camelProperties.entrySet().stream().map(this::filterSensitive).collect(Collectors.toList());
             LOG.info("Setting initial properties in Camel context: [{}]", filteredProps);
             camelMain.setInitialProperties(camelProperties);
 
-            //error handler
-            camelMain.getCamelContext().getRegistry().bind("ckcErrorHandler", new DefaultErrorHandlerBuilder());
-            if (errorHandler != null) {
-                switch (errorHandler) {
-                    case "no":
-                        camelMain.getCamelContext().getRegistry().bind("ckcErrorHandler", new NoErrorHandlerBuilder());
-                        break;
-                    case "default":
-                        camelMain.getCamelContext().getRegistry().bind("ckcErrorHandler", new DefaultErrorHandlerBuilder().maximumRedeliveries(maxRedeliveries).redeliveryDelay(redeliveryDelay));
-                        break;
-                    default:
-                        break;
-                }
-            }
-
             camelMain.configure().addRoutesBuilder(new RouteBuilder() {
                 public void configure() {
 
-                    //creating source template
-                    RouteTemplateDefinition rtdSource = routeTemplate("ckcSource")
-                            .templateParameter("fromUrl")
-                            .templateParameter("errorHandler", "ckcErrorHandler")
-
+                    //create marshal template
+                    routeTemplate("ckcMarshal")
                             .templateParameter("marshal", "dummyDataformat")
-                            .templateParameter("unmarshal", "dummyDataformat")
+                            .from("kamelet:source")
+                            .marshal("{{marshal}}")
+                            .to("kamelet:sink");
 
-                            //TODO: change CamelConnectorConfig.CAMEL_CONNECTOR_AGGREGATE_NA to ckcAggregationStrategy?
+                    //create unmarshal template
+                    routeTemplate("ckcUnMarshal")
+                            .templateParameter("unmarshal", "dummyDataformat")
+                            .from("kamelet:source")
+                            .marshal("{{unmarshal}}")
+                            .to("kamelet:sink");
+
+                    //create aggregator template
+                    routeTemplate("ckcAggregator")
+                            //TODO: change CamelConnectorConfig.CAMEL_CONNECTOR_AGGREGATE_NAME to ckcAggregationStrategy?
                             .templateParameter("aggregationStrategy", CamelConnectorConfig.CAMEL_CONNECTOR_AGGREGATE_NAME)
                             .templateParameter("aggregationSize", "1")
                             .templateParameter("aggregationTimeout", String.valueOf(Long.MAX_VALUE))
+                            .from("kamelet:source")
+                            .aggregate(constant(true))
+                                .aggregationStrategyRef("{{aggregationStrategy}}")
+                                .completionSize("{{aggregationSize}}")
+                                .completionTimeout("{{aggregationTimeout}}")
+                                .to("kamelet:sink")
+                            .end();
 
+                    //create idempotent template
+                    routeTemplate("ckcIdempotent")
                             .templateParameter("idempotentExpression", "dummyExpression")
                             .templateParameter("idempotentRepository", "ckcIdempotentRepository")
-                            .templateParameter("headersExcludePattern", "(?!)");
+                            .from("kamelet:source")
+                            .idempotentConsumer(simple("{{idempotentExpression}}")).messageIdRepositoryRef("{{idempotentRepository}}")
+                            .to("kamelet:sink");
 
+                    //create removeHeader template
+                    routeTemplate("ckcRemoveHeader")
+                            .templateParameter("headersExcludePattern", "(?!)")
+                            .from("kamelet:source")
+                            .removeHeaders("{{headersExcludePattern}}")
+                            .to("kamelet:sink");
 
-                    ProcessorDefinition<?> rdInTemplateSource = rtdSource.from("{{fromUrl}}")
-                            .errorHandler(new ErrorHandlerBuilderRef("{{errorHandler}}"));
-                    if (!ObjectHelper.isEmpty(marshallDataFormat)) {
-                        rdInTemplateSource = rdInTemplateSource.marshal("{{marshal}}");
-                    }
-                    if (!ObjectHelper.isEmpty(unmarshallDataFormat)) {
-                        rdInTemplateSource = rdInTemplateSource.unmarshal("{{unmarshal}}");
-                    }
-
-                    if (getContext().getRegistry().lookupByName("aggregate") != null) {
-                        AggregationStrategy s = getContext().getRegistry().lookupByNameAndType(CamelConnectorConfig.CAMEL_CONNECTOR_AGGREGATE_NAME, AggregationStrategy.class);
-                        rdInTemplateSource = rdInTemplateSource.aggregate(s)
-                                .constant(true)
-                                .completionSize("{{aggregationSize}}")
-                                .completionTimeout("{{aggregationTimeout}}");
-                    }
-
-                    if (idempotencyEnabled) {
-                        rdInTemplateSource = rdInTemplateSource.idempotentConsumer(simple("{{idempotentExpression}}")).messageIdRepositoryRef("{{idempotentRepository}}");
-                    }
-
-                    rdInTemplateSource.removeHeaders("{{headersExcludePattern}}")
+                    //creating source template
+                    routeTemplate("ckcSource")
+                            .templateParameter("fromUrl")
+                            .templateParameter("errorHandler", "ckcErrorHandler")
+                            .from("{{fromUrl}}")
+                            .errorHandler(new ErrorHandlerBuilderRef("{{errorHandler}}"))
                             .to("kamelet:sink");
 
                     //creating sink template
-                    RouteTemplateDefinition rtdSink = routeTemplate("ckcSink")
+                    routeTemplate("ckcSink")
                             .templateParameter("toUrl")
                             .templateParameter("errorHandler", "ckcErrorHandler")
-                            .templateParameter("marshal", "dummyDataformat")
-                            .templateParameter("unmarshal", "dummyDataformat")
-
-                            //TODO: change CamelConnectorConfig.CAMEL_CONNECTOR_AGGREGATE_NA to ckcAggregationStrategy?
-                            .templateParameter("aggregationStrategy", CamelConnectorConfig.CAMEL_CONNECTOR_AGGREGATE_NAME)
-                            .templateParameter("aggregationSize", "1")
-                            .templateParameter("aggregationTimeout", String.valueOf(Long.MAX_VALUE))
-
-                            .templateParameter("idempotentExpression", "dummyExpression")
-                            .templateParameter("idempotentRepository", "ckcIdempotentRepository")
-                            .templateParameter("headersExcludePattern", "(?!)");
-
-
-                    ProcessorDefinition<?> rdInTemplateSink = rtdSink.from("kamelet:source")
-                            .errorHandler(new ErrorHandlerBuilderRef("{{errorHandler}}"));
-                    if (!ObjectHelper.isEmpty(marshallDataFormat)) {
-                        rdInTemplateSink = rdInTemplateSink.marshal("{{marshal}}");
-                    }
-                    if (!ObjectHelper.isEmpty(unmarshallDataFormat)) {
-                        rdInTemplateSink = rdInTemplateSink.unmarshal("{{unmarshal}}");
-                    }
-
-                    if (getContext().getRegistry().lookupByName("aggregate") != null) {
-                        AggregationStrategy s = getContext().getRegistry().lookupByNameAndType(CamelConnectorConfig.CAMEL_CONNECTOR_AGGREGATE_NAME, AggregationStrategy.class);
-                        rdInTemplateSink = rdInTemplateSink.aggregate(s)
-                                .constant(true)
-                                .completionSize("{{aggregationSize}}")
-                                .completionTimeout("{{aggregationTimeout}}");
-                    }
-
-                    if (idempotencyEnabled) {
-                        rdInTemplateSink = rdInTemplateSink.idempotentConsumer(simple("{{idempotentExpression}}")).messageIdRepositoryRef("{{idempotentRepository}}");
-                    }
-
-                    rdInTemplateSink.removeHeaders("{{headersExcludePattern}}")
+                            .from("kamelet:source")
+                            .errorHandler(new ErrorHandlerBuilderRef("{{errorHandler}}"))
                             .to("{{toUrl}}");
 
                     //creating the actual route
-                    from(from).toD(to);
+                    ProcessorDefinition<?> rd = from(from);
+                    if (!ObjectHelper.isEmpty(marshallDataFormat)) {
+                        rd = rd.kamelet("ckcMarshal");
+                    }
+                    if (!ObjectHelper.isEmpty(unmarshallDataFormat)) {
+                        rd = rd.kamelet("ckcUnMarshal");
+                    }
+                    if (getContext().getRegistry().lookupByName("aggregate") != null) {
+                        rd = rd.kamelet("ckcAggregator");
+                    }
+                    if (idempotencyEnabled) {
+                        rd = rd.kamelet("ckcIdempotent");
+                    }
+                    rd = rd.kamelet("ckcRemoveHeader");
+                    rd.toD(to);
                 }
             });
 

core/src/test/java/org/apache/camel/kafkaconnector/DataFormatTest.java
@@ -16,9 +16,7 @@
  */
 package org.apache.camel.kafkaconnector;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.apache.camel.component.hl7.HL7DataFormat;
@@ -26,7 +24,6 @@ import org.apache.camel.component.syslog.SyslogDataFormat;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.kafkaconnector.utils.CamelKafkaConnectMain;
 import org.apache.kafka.connect.errors.ConnectException;
-import org.apache.kafka.connect.sink.SinkRecord;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -69,11 +66,7 @@ public class DataFormatTest {
         props.put("camel.sink.marshal", "missingDataformat");
 
         CamelSinkTask camelsinkTask = new CamelSinkTask();
-        camelsinkTask.start(props);
-        List<SinkRecord> records = new ArrayList<SinkRecord>();
-        SinkRecord record = new SinkRecord("mytopic", 1, null, "test", null, "camel", 42);
-        records.add(record);
-        assertThrows(ConnectException.class, () -> camelsinkTask.put(records));
+        assertThrows(ConnectException.class, () -> camelsinkTask.start(props));
         // No need to check the stop method. The error is already thrown/caught during startup.
         camelsinkTask.stop();
     }

commit c5a48edbea7d0bd1cf9a28d37d3ab8876ce42d0c

Fix #1795: ckcUnMarshal route template applies unmarshal instead of marshal
The ckcUnMarshal route template was calling .marshal("{{unmarshal}}"), so every
connector configured with camel.sink.unmarshal or camel.source.unmarshal got a
route that marshalled the payload instead of unmarshalling it.

This is a regression from ca4e784352, which extracted the marshal/unmarshal
steps into standalone route templates; before that commit the route was built
with .unmarshal(unmarshallDataFormat) directly.

The existing DataFormatTest cases only assert that the context starts and the
data format resolves, so the defect passed CI. Add two tests that send a message
through the route with a data format that records which direction it was invoked
in, asserting that camel.*.unmarshal unmarshals and camel.*.marshal marshals.

Co-Authored-By: Claude Opus 5 (1M context) <noreply@anthropic.com>
Signed-off-by: Andrea Cosentino <ancosen@gmail.com>

core/src/main/java/org/apache/camel/kafkaconnector/utils/CamelKafkaConnectMain.java
@@ -316,7 +316,7 @@ public class CamelKafkaConnectMain extends SimpleMain {
                     routeTemplate("ckcUnMarshal")
                             .templateParameter("unmarshal", "dummyDataformat")
                             .from("kamelet:source")
-                            .marshal("{{unmarshal}}")
+                            .unmarshal("{{unmarshal}}")
                             .to("kamelet:sink");
 
                     //create aggregator template

core/src/test/java/org/apache/camel/kafkaconnector/DataFormatTest.java
@@ -16,19 +16,25 @@
  */
 package org.apache.camel.kafkaconnector;
 
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.component.hl7.HL7DataFormat;
 import org.apache.camel.component.syslog.SyslogDataFormat;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.kafkaconnector.utils.CamelKafkaConnectMain;
+import org.apache.camel.spi.DataFormat;
+import org.apache.camel.support.service.ServiceSupport;
 import org.apache.kafka.connect.errors.ConnectException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DataFormatTest {
 
@@ -143,5 +149,79 @@ public class DataFormatTest {
         HL7DataFormat hl7dfLoaded = (HL7DataFormat)dcc.resolveDataFormat("hl7");
         assertFalse(hl7dfLoaded.isValidate());
         cms.stop();
+    }
+
+    @Test
+    public void testUnmarshalDataFormatIsAppliedInTheUnmarshalDirection() throws Exception {
+        DirectionRecordingDataFormat dataFormat = new DirectionRecordingDataFormat();
+        DefaultCamelContext dcc = new DefaultCamelContext();
+        dcc.getRegistry().bind("directionRecording", dataFormat);
+
+        CamelKafkaConnectMain cms = CamelKafkaConnectMain.builder("direct://start", "log://test")
+            .withProperties(new HashMap<String, String>())
+            .withUnmarshallDataFormat("directionRecording")
+            .build(dcc);
+
+        cms.start();
+        cms.getProducerTemplate().sendBody("direct://start", "payload");
+        cms.stop();
+
+        assertTrue(dataFormat.isUnmarshalled(), "camel.*.unmarshal must apply the data format in the unmarshal direction");
+        assertFalse(dataFormat.isMarshalled(), "camel.*.unmarshal must not apply the data format in the marshal direction");
+    }
+
+    @Test
+    public void testMarshalDataFormatIsAppliedInTheMarshalDirection() throws Exception {
+        DirectionRecordingDataFormat dataFormat = new DirectionRecordingDataFormat();
+        DefaultCamelContext dcc = new DefaultCamelContext();
+        dcc.getRegistry().bind("directionRecording", dataFormat);
+
+        CamelKafkaConnectMain cms = CamelKafkaConnectMain.builder("direct://start", "log://test")
+            .withProperties(new HashMap<String, String>())
+            .withMarshallDataFormat("directionRecording")
+            .build(dcc);
+
+        cms.start();
+        cms.getProducerTemplate().sendBody("direct://start", "payload");
+        cms.stop();
+
+        assertTrue(dataFormat.isMarshalled(), "camel.*.marshal must apply the data format in the marshal direction");
+        assertFalse(dataFormat.isUnmarshalled(), "camel.*.marshal must not apply the data format in the unmarshal direction");
+    }
+
+    /**
+     * A data format that only records which direction it was invoked in, so that a test can assert that the route
+     * template applies the operation the configuration actually names.
+     */
+    private static final class DirectionRecordingDataFormat extends ServiceSupport implements DataFormat {
+
+        private boolean marshalled;
+        private boolean unmarshalled;
+
+        @Override
+        public void marshal(Exchange exchange, Object graph, OutputStream stream) throws Exception {
+            marshalled = true;
+            stream.write(exchange.getContext().getTypeConverter().mandatoryConvertTo(byte[].class, exchange, graph));
+        }
+
+        @Override
+        public Object unmarshal(Exchange exchange, InputStream stream) throws Exception {
+            unmarshalled = true;
+            return exchange.getContext().getTypeConverter().mandatoryConvertTo(String.class, exchange, stream);
+        }
+
+        @Override
+        public Object unmarshal(Exchange exchange, Object body) throws Exception {
+            unmarshalled = true;
+            return exchange.getContext().getTypeConverter().mandatoryConvertTo(String.class, exchange, body);
+        }
+
+        boolean isMarshalled() {
+            return marshalled;
+        }
+
+        boolean isUnmarshalled() {
+            return unmarshalled;
+        }
     }
 }